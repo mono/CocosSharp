bf7a29af05d0ecf096c5904bdb42034ffd063b54
TotallyEvil Developers <dev@totallyevil.com>
2013-01-27 22:45:32 -0800

    added the ios main

--- tests/tests/Program.cs
+++ tests/tests/Program.cs
@@ -7,9 +7,35 @@ using Android.OS;
 using Android.Widget;
 using Microsoft.Xna.Framework;
 #endif
+#if IPHONE || IOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
 
 namespace tests
 {
+#if IPHONE || IOS
+	[Register ("AppDelegate")]
+	class Program : UIApplicationDelegate 
+	{
+		private Game1 game;
+
+		public override void FinishedLaunching (UIApplication app)
+		{
+			// Fun begins..
+			game = new Game1();
+			game.Run();
+		}
+		
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+#endif
 #if WINDOWS || XBOX || PSM
     static class Program
     {
