cf77d778c843f2062c3dae099ecd244f2ccf56c8
TotallyEvil Developers <dev@totallyevil.com>
2013-03-04 23:02:55 -0800

    quick fix for errant interface usage.

--- cocos2d/textures/CCTextureCache.cs
+++ cocos2d/textures/CCTextureCache.cs
@@ -155,22 +155,14 @@ namespace cocos2d
                     // Either we are creating a new one or else we need to refresh the current one.
                     // CCLog.Log("Loading texture {0}", fileimage);
                     
-#if IOS || IPHONE
-					Texture2D textureXna = CCApplication.SharedApplication.Content.Load(imageStream, assetName);
-#else
                     Texture2D textureXna = Texture2D.FromStream(CCApplication.SharedApplication.GraphicsDevice, imageStream);
-#endif
 
                     bool isInited = texture.InitWithTexture(textureXna);
                     
                     if (isInited)
                     {
-#if IOS || IPHONE
-                        texture.IsManaged = true;
-#else
-							// Can only be true if the content manager loads the texture.
+						// Can only be true if the content manager loads the texture.
                         texture.IsManaged = false;
-#endif
                         texture.ContentFile = assetName;
                         m_pTextures[pathKey] = texture;
                         m_pTextureRefNames[texture] = assetName;
