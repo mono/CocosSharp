ba5dca23a5d94d9c665d045f5fa491cc18106f7f
jacob anderson <jwa@totallyevil.com>
2013-03-06 07:36:09 -0800

    fixed name of image
    on device the names of assets is case sensitive.

--- tests/tests/classes/tests/ParticleTest/ParticleTest.cs
+++ tests/tests/classes/tests/ParticleTest/ParticleTest.cs
@@ -213,7 +213,7 @@ namespace tests
 
             AddChild(menu, 100);
 
-            CCLabelAtlas labelAtlas = CCLabelAtlas.Create("0000", "Images/fps_images", 16, 24, '.');
+            CCLabelAtlas labelAtlas = CCLabelAtlas.Create("0000", "Images/fps_Images", 16, 24, '.');
             AddChild(labelAtlas, 100, ParticleTestScene.kTagLabelAtlas);
             labelAtlas.Position = new CCPoint(s.Width - 66, 50);
 
