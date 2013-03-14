4faa142bfaab92c074e98195da55fc65dd822880
kjpou1 <kjpou@pt.lu>
2013-03-14 22:35:38 +0100

    Fix for loading of CCBMFontConfiguration - i.e. .FNT files.

---
+++ cocos2d/platform/ContentReaders/CCRectReader.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Xna.Framework.Content;
+
+namespace cocos2d
+{
+    public class CCRectReader : ContentTypeReader<CCRect>
+    {
+        public CCRectReader()
+        {
+        }
+
+        protected override CCRect Read (ContentReader input, CCRect existingInstance)
+        {
+            var x = input.ReadSingle ();
+            var y = input.ReadSingle ();
+            var width = input.ReadSingle ();
+            var height = input.ReadSingle ();
+
+            var objectRect = new CCRect(x, y, width, height);
+
+            return objectRect;
+        }
+
+    }
+}
+
