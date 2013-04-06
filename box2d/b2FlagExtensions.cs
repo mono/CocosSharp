99845b2a5d8f47f47ee0d6b1d3e3f33cf119c237
TotallyEvil Developers <dev@totallyevil.com>
2013-04-05 23:58:42 -0700

    this is required for the xbox build.

---
+++ box2d/b2FlagExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Box2D.Collision;
+using Box2D.Dynamics;
+using Box2D.Common;
+using Box2D.Collision.Shapes;
+using Box2D.Dynamics.Contacts;
+using Box2D.Dynamics.Joints;
+
+namespace Box2D
+{
+#if XBOX
+    public static class b2FlagExtensions
+    {
+            public static bool HasFlag(this b2BodyFlags flag, b2BodyFlags testFlag)
+            {
+                return ((flag & testFlag) == testFlag);
+            }
+
+            public static bool HasFlag(this b2WorldFlags flag, b2WorldFlags testFlag)
+            {
+                return ((flag & testFlag) == testFlag);
+            }
+
+            public static bool HasFlag(this b2ContactFlags flag, b2ContactFlags testFlag)
+            {
+                return ((flag & testFlag) == testFlag);
+            }
+
+            public static bool HasFlag(this b2DrawFlags flag, b2DrawFlags testFlag)
+            {
+                return ((flag & testFlag) == testFlag);
+            }
+    }
+#endif
+}
