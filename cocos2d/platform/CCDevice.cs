4d062dd1409398211627e9dc7e380c3ced9795b1
gena_m <gena_m@list.ru>
2013-05-29 14:58:15 +0500

    add CCDevice class

---
+++ cocos2d/platform/CCDevice.cs
@@ -0,0 +1,37 @@
+﻿#if ANDROID
+using Android.App;
+using Android.Util;
+#endif
+
+#if WINDOWS
+
+#endif
+
+namespace Cocos2D
+{
+
+    public static class CCDevice
+    {
+        private static float _dpi;
+
+        public static float GetDPI()
+        {
+            if (_dpi == 0)
+            {
+#if ANDROID
+                var contex = (Activity)CCApplication.SharedApplication.Game.Window.Context;
+                var display = contex.WindowManager.DefaultDisplay;
+                var metrics = new DisplayMetrics();
+            
+                display.GetMetrics(metrics);
+
+                _dpi = metrics.Density * 160.0f;
+#else
+                //TODO: Implementing GetDPI for all platforms
+                _dpi = 96;
+#endif
+            }
+            return _dpi;
+        }
+    }
+}
