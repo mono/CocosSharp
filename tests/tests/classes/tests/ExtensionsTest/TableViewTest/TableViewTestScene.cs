127ed43c7925b7bd952661f7a170197daf977e3d
gena_m <gena_m@list.ru>
2013-05-29 15:37:24 +0500

    TableView test sync with the latest version of cocos2d-x

--- tests/tests/classes/tests/ExtensionsTest/TableViewTest/TableViewTestScene.cs
+++ tests/tests/classes/tests/ExtensionsTest/TableViewTest/TableViewTestScene.cs
@@ -3,118 +3,123 @@ using Cocos2D;
 
 namespace tests.Extensions
 {
-	public class TableViewTestLayer : CCLayer, CCTableViewDataSource, CCTableViewDelegate
-	{
-		public static void runTableViewTest()
-		{
-			var pScene = new CCScene();
-			var pLayer = new TableViewTestLayer();
-			pLayer.Init();
-			pScene.AddChild(pLayer);
-			CCDirector.SharedDirector.ReplaceScene(pScene);
-		}
+    public class TableViewTestLayer : CCLayer, CCTableViewDataSource, CCTableViewDelegate
+    {
+        public static void runTableViewTest()
+        {
+            var pScene = new CCScene();
+            var pLayer = new TableViewTestLayer();
+            pLayer.Init();
+            pScene.AddChild(pLayer);
+            CCDirector.SharedDirector.ReplaceScene(pScene);
+        }
 
-		public override bool Init()
-		{
-			if (!base.Init())
-				return false;
+        public override bool Init()
+        {
+            if (!base.Init())
+                return false;
 
-			var winSize = CCDirector.SharedDirector.WinSize;
+            var winSize = CCDirector.SharedDirector.WinSize;
 
-			var tableView = new CCTableView(this, new CCSize(250, 60));
-			tableView.Direction = CCScrollViewDirection.Horizontal;
-			tableView.Position = new CCPoint(20,winSize.Height/2-30);
-			tableView.Delegate = this;
-			this.AddChild(tableView);
-			tableView.ReloadData();
+            var tableView = new CCTableView(this, new CCSize(250, 60));
+            tableView.Direction = CCScrollViewDirection.Horizontal;
+            tableView.Position = new CCPoint(20, winSize.Height / 2 - 30);
+            tableView.Delegate = this;
+            this.AddChild(tableView);
+            tableView.ReloadData();
 
-			tableView = new CCTableView(this, new CCSize(60, 280));
-			tableView.Direction = CCScrollViewDirection.Vertical;
-			tableView.Position = new CCPoint(winSize.Width - 150, winSize.Height/2 - 120);
-			tableView.Delegate = this;
-			tableView.VerticalFillOrder = CCTableViewVerticalFillOrder.FillTopDown;
-			this.AddChild(tableView);
-			tableView.ReloadData();
+            tableView = new CCTableView(this, new CCSize(60, 280));
+            tableView.Direction = CCScrollViewDirection.Vertical;
+            tableView.Position = new CCPoint(winSize.Width - 150, winSize.Height / 2 - 120);
+            tableView.Delegate = this;
+            tableView.VerticalFillOrder = CCTableViewVerticalFillOrder.FillTopDown;
+            this.AddChild(tableView);
+            tableView.ReloadData();
 
-			// Back Menu
-			var itemBack = new CCMenuItemFont("Back", toExtensionsMainLayer);
-			itemBack.Position = new CCPoint(winSize.Width - 50, 25);
-			var menuBack = new CCMenu(itemBack);
-			menuBack.Position = CCPoint.Zero;
-			AddChild(menuBack);
+            // Back Menu
+            var itemBack = new CCMenuItemFont("Back", toExtensionsMainLayer);
+            itemBack.Position = new CCPoint(winSize.Width - 50, 25);
+            var menuBack = new CCMenu(itemBack);
+            menuBack.Position = CCPoint.Zero;
+            AddChild(menuBack);
 
-			return true;
-		}
-   
-		public void toExtensionsMainLayer(object sender)
-		{
-			var pScene = new ExtensionsTestScene();
-			pScene.runThisTest();
-		}
+            return true;
+        }
 
-		//CREATE_FUNC(TableViewTestLayer);
-    
-		public virtual void ScrollViewDidScroll(CCScrollView view)
-		{
-		}
+        public void toExtensionsMainLayer(object sender)
+        {
+            var pScene = new ExtensionsTestScene();
+            pScene.runThisTest();
+        }
 
-		public virtual void ScrollViewDidZoom(CCScrollView view)
-		{
-		}
+        //CREATE_FUNC(TableViewTestLayer);
 
-		public virtual void TableCellTouched(CCTableView table, CCTableViewCell cell)
-		{
+        public virtual void ScrollViewDidScroll(CCScrollView view)
+        {
+        }
+
+        public virtual void ScrollViewDidZoom(CCScrollView view)
+        {
+        }
+
+        public virtual void TableCellTouched(CCTableView table, CCTableViewCell cell)
+        {
             CCLog.Log("cell touched at index: {0}", cell.Index);
-		}
+        }
 
-	    public void TableCellHighlight(CCTableView table, CCTableViewCell cell)
-	    {
-	    }
+        public void TableCellHighlight(CCTableView table, CCTableViewCell cell)
+        {
+        }
 
-	    public void TableCellUnhighlight(CCTableView table, CCTableViewCell cell)
-	    {
-	    }
+        public void TableCellUnhighlight(CCTableView table, CCTableViewCell cell)
+        {
+        }
 
-	    public void TableCellWillRecycle(CCTableView table, CCTableViewCell cell)
-	    {
-	    }
+        public void TableCellWillRecycle(CCTableView table, CCTableViewCell cell)
+        {
+        }
 
-	    public CCSize TableCellSizeForIndex(CCTableView table, int idx)
-	    {
+        public CCSize TableCellSizeForIndex(CCTableView table, int idx)
+        {
+            if (idx == 2)
+            {
+                return new CCSize(100, 100);
+            }
             return new CCSize(60, 60);
         }
 
-	    public virtual CCTableViewCell TableCellAtIndex(CCTableView table, int idx)
-		{
-			string str = idx.ToString();
-			var cell = table.DequeueCell();
-			
-			if (cell == null) {
-				cell = new CustomTableViewCell();
-				var sprite = new CCSprite("Images/Icon");
-				sprite.AnchorPoint = CCPoint.Zero;
-				sprite.Position = new CCPoint(0, 0);
-				cell.AddChild(sprite);
+        public virtual CCTableViewCell TableCellAtIndex(CCTableView table, int idx)
+        {
+            string str = idx.ToString();
+            var cell = table.DequeueCell();
 
-				var label = new CCLabelTTF(str, "Helvetica", 20.0f);
-				label.Position = CCPoint.Zero;
-				label.AnchorPoint = CCPoint.Zero;
-				label.Tag = 123;
-				cell.AddChild(label);
-			}
-			else
-			{
-				var label = (CCLabelTTF)cell.GetChildByTag(123);
-				label.Label = (str);
-			}
+            if (cell == null)
+            {
+                cell = new CustomTableViewCell();
+                var sprite = new CCSprite("Images/Icon");
+                sprite.AnchorPoint = CCPoint.Zero;
+                sprite.Position = new CCPoint(0, 0);
+                cell.AddChild(sprite);
+
+                var label = new CCLabelTTF(str, "Helvetica", 20.0f);
+                label.Position = CCPoint.Zero;
+                label.AnchorPoint = CCPoint.Zero;
+                label.Tag = 123;
+                cell.AddChild(label);
+            }
+            else
+            {
+                var label = (CCLabelTTF) cell.GetChildByTag(123);
+                label.Label = (str);
+            }
 
 
-			return cell;
-		}
+            return cell;
+        }
 
-		public virtual int NumberOfCellsInTableView(CCTableView table)
-		{
-			return 20;
-		}
-	}
+        public virtual int NumberOfCellsInTableView(CCTableView table)
+        {
+            return 20;
+        }
+    }
 }
