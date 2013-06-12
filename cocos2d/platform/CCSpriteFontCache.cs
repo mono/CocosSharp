78baaa05026fdbfdd21c54f8b33d781d5045cd47
gena_m <gena_m@list.ru>
2013-06-12 16:36:24 +0500

    refactoring CCSpriteFontCache

--- cocos2d/platform/CCSpriteFontCache.cs
+++ cocos2d/platform/CCSpriteFontCache.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.IO;
 
@@ -8,7 +7,7 @@ namespace Cocos2D
 {
     public class CCSpriteFontCache
     {
-        private Dictionary<string, SpriteFont> _Cache = new Dictionary<string, SpriteFont>();
+        private static ContentManager _contentManager;
         public static string FontRoot = "fonts";
 
         private CCSpriteFontCache()
@@ -16,63 +15,50 @@ namespace Cocos2D
             // You don't create this, we do.
         }
 
+        static CCSpriteFontCache()
+        {
+            var cm = CCApplication.SharedApplication.Content;
+            _contentManager = new ContentManager(cm.ServiceProvider, Path.Combine(cm.RootDirectory, FontRoot));
+        }
+
         public SpriteFont GetFont(string fontName)
         {
-            string key = string.Format("{0}/{1}", fontName.ToLower(), "ANY");
-            if (_Cache.ContainsKey(key))
-            {
-                return (_Cache[key]);
-            }
-            SpriteFont font = null;
             try
             {
-                font = CCApplication.SharedApplication.Content.Load<SpriteFont>(Path.Combine("fonts", fontName));
+                return _contentManager.Load<SpriteFont>(fontName);
             }
             catch (Exception)
             {
                 CCLog.Log("Can't find font known as {0}. Please check your file name.", fontName);
+                return null;
             }
-            if (font != null)
-            {
-                _Cache[key] = font;
-            }
-            return (font);
         }
 
         public SpriteFont GetFont(string fontName, float size)
         {
             if (size <= 0f)
             {
-                return (GetFont(fontName));
+                return GetFont(fontName);
             }
-            string key = string.Format("{0}/{1}", fontName.ToLower(), size);
-            if (_Cache.ContainsKey(key))
-            {
-                return (_Cache[key]);
-            }
-            SpriteFont font = null;
+
             try
             {
-                font = CCApplication.SharedApplication.Content.Load<SpriteFont>(Path.Combine("fonts", string.Format("{0}-{1}", fontName, size)));
+                return _contentManager.Load<SpriteFont>(String.Format("{0}-{1}", fontName, size));
             }
             catch (Exception)
             {
                 CCLog.Log("Can't find {0}-{1}, going to try using {0} instead.", fontName, size);
-                return (GetFont(fontName));
+                return GetFont(fontName);
             }
-            if (font != null)
-            {
-                _Cache[key] = font;
-            }
-            return (font);
         }
 
         public void Clear()
         {
-            _Cache.Clear();
+            _contentManager.Unload();
         }
 
         #region Singleton
+        
         public static CCSpriteFontCache SharedInstance
         {
             get
@@ -81,6 +67,7 @@ namespace Cocos2D
             }
         }
         private static CCSpriteFontCache _Instance = new CCSpriteFontCache();
+        
         #endregion
     }
 }
