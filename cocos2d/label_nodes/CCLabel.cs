bb3c20075ad19d5a7af00a3cfa4a420ad2fbf17c
kjpou1 <kjpou@pt.lu>
2013-06-29 20:50:18 +0200

    Add SystemFont support for CoreGraphics implementation.

--- cocos2d/label_nodes/CCLabel.cs
+++ cocos2d/label_nodes/CCLabel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 
--- cocos2d/label_nodes/CCLabelUtilities.cs
+++ cocos2d/label_nodes/CCLabelUtilities.cs
@@ -5,12 +5,12 @@ namespace Cocos2D
 	internal static partial class CCLabelUtilities
 	{
 
-		internal static CCTexture2D CreateLabelTexture (string text, CCSize dimensions, CCTextAlignment hAlignment,
-		                                   CCVerticalTextAlignment vAlignment, string fontName,
-		                                   float fontSize, CCColor4B textColor)
-		{
-			return CreateNativeLabel (text, dimensions, hAlignment, vAlignment, fontName, fontSize, textColor);
-		}
+//		internal static CCTexture2D CreateLabelTexture (string text, CCSize dimensions, CCTextAlignment hAlignment,
+//		                                   CCVerticalTextAlignment vAlignment, string fontName,
+//		                                   float fontSize, CCColor4B textColor)
+//		{
+//			//return CreateNativeLabel (text, dimensions, hAlignment, vAlignment, fontName, fontSize, textColor);
+//		}
 	}
 }
 
