5338a80863bfe24a493c0f1eb8bb157d93ac02a7
Jacob Anderson <jwa@totallyevil.com>
2013-08-01 16:44:20 -0700

    added some nuget publishing scripts

--- scripts/build-Microsoft.bat
+++ scripts/build-Microsoft.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat x86"
 set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;c:\windows\system32;c:\nant-0.92\bin
 nant -buildfile:microsoft.build > microsoft.out 2>&1
-nant -buildfile:deployments.build > deploy.out 2>&1
+nant -buildfile:deploy-Microsoft.build > deploy.out 2>&1
