e1a369010d4cc88a7da6f9a516bbcd1a8caf20bd
Kenneth J. Pouncey <kjpou@pt.lu>
2013-08-08 06:28:14 +0200

    Add Accelerometer support windows phone 8

--- tests/tests/classes/tests/AccelerometerTest/AccelerometerTest.cs
+++ tests/tests/classes/tests/AccelerometerTest/AccelerometerTest.cs
@@ -29,8 +29,10 @@ namespace tests
             CCPoint ptNow = m_pBall.Position;
             CCPoint ptTemp = pDir.ConvertToUi(ptNow);
 
-            ptTemp.X += (float) pAccelerationValue.X * 9.81f;
-            ptTemp.Y -= (float) pAccelerationValue.Y * 9.81f;
+            //CCLog.Log("Accelerate : X: {0} Y: {1} Z: {2}", pAccelerationValue.X, pAccelerationValue.Y, pAccelerationValue.Z);
+
+            ptTemp.X -= (float) pAccelerationValue.Y * 9.81f;
+            ptTemp.Y -= (float) pAccelerationValue.X * 9.81f;
 
             CCPoint ptNext = pDir.ConvertToGl(ptTemp);
             ptNext.X = MathHelper.Clamp(ptNext.X, (ballSize.Width / 2.0f), (winSize.Width - ballSize.Width / 2.0f));
