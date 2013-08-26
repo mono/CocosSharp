e44dc008c021dedc58a3afdef52f50129c1b3d16
gena-m <gena_m@list.ru>
2013-08-26 09:44:04 +0500

    added CCSprite(CCSize size), CCSprite(CCRect rect) constructors

--- cocos2d/sprite_nodes/CCSprite.cs
+++ cocos2d/sprite_nodes/CCSprite.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -448,7 +449,31 @@ namespace Cocos2D
 
         #region Constructors
 
-        public CCSprite (CCTexture2D texture)
+        public CCSprite()
+        {
+            if (!Init())
+            {
+                CCLog.Log("CCSprite (): Problems initializing class");
+            }
+        }
+
+        public CCSprite(CCSize size)
+        {
+            if (!InitWithTexture(null, new CCRect(0, 0, size.Width, size.Height)))
+            {
+                CCLog.Log("CCSprite (CCSize size): Problems initializing class");
+            }
+        }
+
+        public CCSprite(CCRect rect)
+        {
+            if (!InitWithTexture(null, rect))
+            {
+                CCLog.Log("CCSprite (CCRect rect): Problems initializing class");
+            }
+        }
+
+        public CCSprite(CCTexture2D texture)
         {
             if (!InitWithTexture(texture))
             {
@@ -486,14 +511,6 @@ namespace Cocos2D
             if (!InitWithSpriteFrame(pSpriteFrame))
             {
 				CCLog.Log("CCSprite (CCSpriteFrame pSpriteFrame): Problems initializing class"); 
-			}
-        }
-
-        public CCSprite ()
-        {
-            if (!Init())
-            {
-				CCLog.Log("CCSprite (): Problems initializing class"); 
 			}
         }
 
