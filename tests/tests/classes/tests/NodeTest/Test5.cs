970afeb3b8b2fb2575b36a0cb3dd9f779ca26349
Jacob Anderson <jwa@totallyevil.com>
2013-09-08 22:23:27 -0700

    added some optional testing code for test 5.

--- tests/tests/classes/tests/NodeTest/Test5.cs
+++ tests/tests/classes/tests/NodeTest/Test5.cs
@@ -4,10 +4,13 @@ namespace tests
 {
     public class Test5 : TestCocosNodeDemo
     {
+        private CCSprite sp2;
+        private CCAction forever2;
+
         public Test5()
         {
             CCSprite sp1 = new CCSprite(TestResource.s_pPathSister1);
-            CCSprite sp2 = new CCSprite(TestResource.s_pPathSister2);
+            sp2 = new CCSprite(TestResource.s_pPathSister2);
 
             sp1.Position = (new CCPoint(100, 160));
             sp2.Position = (new CCPoint(380, 160));
@@ -15,20 +18,34 @@ namespace tests
             CCRotateBy rot = new CCRotateBy (2, 360);
             var rot_back = rot.Reverse() as CCActionInterval;
             CCAction forever = new CCRepeatForever ((CCActionInterval)new CCSequence(rot, rot_back));
-            var forever2 = (CCAction) (forever.Copy());
+            forever2 = (CCAction) (forever.Copy());
             forever.Tag = (101);
             forever2.Tag = (102);
 
             AddChild(sp1, 0, CocosNodeTestStaticLibrary.kTagSprite1);
             AddChild(sp2, 0, CocosNodeTestStaticLibrary.kTagSprite2);
 
-            //initialization Cleanup() errors
             RemoveChild(sp2, true);
             AddChild(sp2, 0, CocosNodeTestStaticLibrary.kTagSprite2);
+
+            // Sprite 1 should run and run
+            // Sprite 2 should stop
 
             sp1.RunAction(forever);
             sp2.RunAction(forever2);
 
+            // Experiment with removing sp2 and re-adding it after cleanup to reproduce an error in child management
+//            ScheduleOnce(Stage2OfTest, 2.0f);
+            Schedule(addAndRemove, 2.0f);
+        }
+
+        public void Stage2OfTest(float dt)
+        {
+            //initialization Cleanup() errors
+            CCLog.Log("Node test #5, stage 2, remove right side sprite and re-add it");
+            RemoveChild(sp2, true);
+            AddChild(sp2, 0, CocosNodeTestStaticLibrary.kTagSprite2);
+            sp2.RunAction(forever2.Copy());
             Schedule(addAndRemove, 2.0f);
         }
 
@@ -42,6 +59,11 @@ namespace tests
 
             AddChild(sp1, 0, CocosNodeTestStaticLibrary.kTagSprite1);
             AddChild(sp2, 0, CocosNodeTestStaticLibrary.kTagSprite2);
+        }
+
+        public override string subtitle()
+        {
+            return ("test #5");
         }
 
         public override string title()
