0e2127c34b9d8a4db238aa5976714097f1b6c1ca
gena-m <gena_m@list.ru>
2013-09-13 11:36:17 +0500

    use predefined SamplerState

--- cocos2d/textures/CCTexture2D.cs
+++ cocos2d/textures/CCTexture2D.cs
@@ -175,19 +175,35 @@ namespace Cocos2D
         {
             var saveState = m_samplerState;
 
-            if (m_bAntialiased)
+            if (m_bAntialiased && m_samplerState.Filter != TextureFilter.Linear)
             {
+                if (m_samplerState == SamplerState.PointClamp)
+                {
+                    m_samplerState = SamplerState.LinearClamp;
+                    return;
+                }
+
                 m_samplerState = new SamplerState
                 {
                     Filter = TextureFilter.Linear
                 };
             }
-            else
+            else if (!m_bAntialiased && m_samplerState.Filter != TextureFilter.Point)
             {
+                if (m_samplerState == SamplerState.LinearClamp)
+                {
+                    m_samplerState = SamplerState.PointClamp;
+                    return;
+                }
+                
                 m_samplerState = new SamplerState
                 {
                     Filter = TextureFilter.Point
                 };
+            }
+            else
+            {
+                return;
             }
 
             m_samplerState.AddressU = saveState.AddressU;
