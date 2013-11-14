233f8d5c3cbd3ea51f4ce1006e77633818c68b44
Jacob Anderson <jwa@totallyevil.com>
2013-11-14 11:52:35 -0800

    build file updates to fix some pathing problems in the master deployment script for nuget packing.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat x86"
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;c:\windows\system32
 nant -buildfile:android.build > android.out 2>&1
 nant -buildfile:microsoft.build > microsoft.out 2>&1
