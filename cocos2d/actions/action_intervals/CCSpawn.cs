72083d0ae6127e6ce845d8b1db653aca15dcc55e
kjpou1 <kjpou@pt.lu>
2014-02-13 07:54:26 +0100

    Separate Action State in CCSpawn.

--- cocos2d/actions/action_intervals/CCSpawn.cs
+++ cocos2d/actions/action_intervals/CCSpawn.cs
@@ -5,8 +5,8 @@ namespace CocosSharp
 {
     public class CCSpawn : CCActionInterval
     {
-        protected CCFiniteTimeAction m_pOne;
-        protected CCFiniteTimeAction m_pTwo;
+		public CCFiniteTimeAction ActionOne { get; protected set; }
+		public CCFiniteTimeAction ActionTwo { get; protected set; }
 
 
         #region Constructors
@@ -27,6 +27,7 @@ namespace CocosSharp
             }
             else
             {
+				// We create a nested set of CCSpawnActions out of all of the actions
                 for (int i = 1; i < actions.Length - 1; i++)
                 {
                     prev = new CCSpawn(prev, actions[i]);
@@ -44,14 +45,6 @@ namespace CocosSharp
             }
         }
 
-        protected CCSpawn(CCSpawn spawn) : base(spawn)
-        {
-            CCFiniteTimeAction param1 = new CCFiniteTimeAction(spawn.m_pOne);
-            CCFiniteTimeAction param2 = new CCFiniteTimeAction(spawn.m_pTwo);
-
-            InitCCSpawn(param1, param2);
-        }
-
         private void InitCCSpawn(CCFiniteTimeAction action1, CCFiniteTimeAction action2)
         {
             Debug.Assert(action1 != null);
@@ -60,57 +53,98 @@ namespace CocosSharp
             float d1 = action1.Duration;
             float d2 = action2.Duration;
 
-            m_pOne = action1;
-            m_pTwo = action2;
+            ActionOne = action1;
+            ActionTwo = action2;
 
             if (d1 > d2)
             {
-                m_pTwo = new CCSequence(action2, new CCDelayTime(d1 - d2));
+                ActionTwo = new CCSequence(action2, new CCDelayTime(d1 - d2));
             }
             else if (d1 < d2)
             {
-                m_pOne = new CCSequence(action1, new CCDelayTime(d2 - d1));
+                ActionOne = new CCSequence(action1, new CCDelayTime(d2 - d1));
             }
         }
 
         #endregion Constructors
 
+		protected internal override CCActionState StartAction (CCNode target)
+		{
+			return new CCSpawnState (this, target);
 
-        public override object Copy(ICCCopyable zone)
-        {
-            return new CCSpawn(this);
-        }
+		}
 
-        protected internal override void StartWithTarget(CCNode target)
-        {
-            base.StartWithTarget(target);
-            m_pOne.StartWithTarget(target);
-            m_pTwo.StartWithTarget(target);
-        }
-
-        public override void Stop()
-        {
-            m_pOne.Stop();
-            m_pTwo.Stop();
-            base.Stop();
-        }
-
-        public override void Update(float time)
-        {
-            if (m_pOne != null)
-            {
-                m_pOne.Update(time);
-            }
-
-            if (m_pTwo != null)
-            {
-                m_pTwo.Update(time);
-            }
-        }
+		public override bool HasState {
+			get {
+				return true;
+			}
+		}
 
         public override CCFiniteTimeAction Reverse()
         {
-            return new CCSpawn(m_pOne.Reverse(), m_pTwo.Reverse());
+            return new CCSpawn(ActionOne.Reverse(), ActionTwo.Reverse());
         }
     }
+
+	public class CCSpawnState : CCActionIntervalState
+	{
+
+		protected CCFiniteTimeAction ActionOne { get; set; }
+		private CCFiniteTimeActionState ActionStateOne { get; set; }
+		protected CCFiniteTimeAction ActionTwo { get; set; }
+		private CCFiniteTimeActionState ActionStateTwo { get; set; }
+
+		public CCSpawnState (CCSpawn action, CCNode target)
+			: base(action, target)
+		{ 
+			ActionOne = action.ActionOne;
+			ActionTwo = action.ActionTwo;
+
+			if (!ActionOne.HasState)
+				ActionOne.StartWithTarget (target);
+			else
+				ActionStateOne = (CCFiniteTimeActionState)ActionOne.StartAction (target);
+
+			if (!ActionTwo.HasState)
+				ActionTwo.StartWithTarget (target);
+			else
+				ActionStateTwo = (CCFiniteTimeActionState)ActionTwo.StartAction (target);
+		}
+
+		public override void Stop()
+		{
+			if (ActionStateOne == null)
+				ActionOne.Stop();
+			else
+				ActionStateOne.Stop();
+
+			if (ActionStateTwo == null)
+				ActionTwo.Stop ();
+			else
+				ActionStateTwo.Stop ();
+
+			base.Stop();
+		}
+
+		public override void Update(float time)
+		{
+			if (ActionOne != null)
+			{
+				if (ActionStateOne == null)
+					ActionOne.Update (time);
+				else
+					ActionStateOne.Update (time);
+			}
+
+			if (ActionTwo != null)
+			{
+				if (ActionStateTwo == null)
+					ActionTwo.Update (time);
+				else
+					ActionStateTwo.Update (time);
+			}
+		}
+
+	}
+
 }
