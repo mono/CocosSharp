aa08efd99bf8b21bf501c8d012186c4ea188202b
kjpou1 <kjpou@pt.lu>
2014-02-16 09:49:40 +0100

    Remove duplicate using statement

--- cocos2d/platform/CCContentManager.cs
+++ cocos2d/platform/CCContentManager.cs
@@ -6,7 +6,6 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
 
 namespace CocosSharp
 {
