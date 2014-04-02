bc6d9e22457b0c43964013269a25ce98c7296727
Rami Tabbara <rtabbara@gmail.com>
2014-04-01 22:37:09 -0700

    Cleanup of CCTMXObjectGroup
    * replacing ivars with auto-implemented properties
    * other general cleanup

--- src/tileMap_parallax_nodes/CCTMXObjectGroup.cs
+++ src/tileMap_parallax_nodes/CCTMXObjectGroup.cs
@@ -4,91 +4,48 @@ namespace CocosSharp
 {
     public class CCTMXObjectGroup 
     {
-        protected List<Dictionary<string, string>> m_pObjects;
-        protected Dictionary<string, string> m_pProperties;
+		#region Properties
 
-        /// <summary>
-        /// name of the group
-        /// </summary>
-        protected string m_sGroupName;
+		public string GroupName { get; set; }
 
-        protected CCPoint m_tPositionOffset;
+		public CCPoint PositionOffset { get; set; }		// offset position of child objects
 
-        /// <summary>
-        /// offset position of child objects
-        /// </summary>
-        public CCPoint PositionOffset
-        {
-            get { return m_tPositionOffset; }
-            set { m_tPositionOffset = value; }
-        }
+		public Dictionary<string, string> Properties { get; set; }
+		public List<Dictionary<string, string>> Objects { get; set; }
 
-        /// <summary>
-        /// list of properties stored in a dictionary
-        /// </summary>
-        public Dictionary<string, string> Properties
-        {
-            get { return m_pProperties; }
-            set { m_pProperties = value; }
-        }
-
-        /// <summary>
-        /// array of the objects
-        /// </summary>
-        public List<Dictionary<string, string>> Objects
-        {
-            get { return m_pObjects; }
-            set { m_pObjects = value; }
-        }
-
-        public string GroupName
-        {
-            get { return m_sGroupName; }
-            set { m_sGroupName = value; }
-        }
+		#endregion Properties
 
 
         #region Constructors
 
         public CCTMXObjectGroup()
         {
-            m_pObjects = new List<Dictionary<string, string>>();
-            m_pProperties = new Dictionary<string, string>();
+			Objects = new List<Dictionary<string, string>>();
+			Properties = new Dictionary<string, string>();
         }
 
         #endregion Constructors
 
 
-        /// <summary>
-        ///  return the value for the specific property name
-        /// </summary>
-        /// <param name="propertyName"></param>
-        /// <returns></returns>
         public string PropertyNamed(string propertyName)
         {
-            return m_pProperties[propertyName];
+			return Properties[propertyName];
         }
 
-        /// <summary>
-        ///  return the dictionary for the specific object name.
-        ///  It will return the 1st object found on the array for the given name.
-        /// </summary>
-        /// <param name="objectName"></param>
-        /// <returns></returns>
         public Dictionary<string, string> ObjectNamed(string objectName)
         {
-            if (m_pObjects != null && m_pObjects.Count > 0)
+			if (Objects != null && Objects.Count > 0)
             {
-                for (int i = 0; i < m_pObjects.Count; i++)
+				foreach (Dictionary<string, string> tmxObjDict in Objects)
                 {
-                    string name = m_pObjects[i]["name"];
-                    if (name != null && name == objectName)
+					string name = tmxObjDict["name"];
+                    if (name == objectName)
                     {
-                        return m_pObjects[i];
+						return tmxObjDict;
                     }
                 }
             }
-            // object not found
+
             return null;
         }
     }
