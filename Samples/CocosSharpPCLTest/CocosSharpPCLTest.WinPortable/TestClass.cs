30322255622faa541537fbb395f65c6b15996d02
Kenneth J. Pouncey <kjpou@pt.lu>
2014-04-17 11:39:23 +0200

    Add CocosSharp PCL Sample for Windows.
    + Add Windows Portable PCL project that.
    + Add Windows Phone 8 project.

---
+++ Samples/CocosSharpPCLTest/CocosSharpPCLTest.WinPortable/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CocosSharp;
+
+namespace CocosSharpPCLTest
+{
+	public static class TestClass
+	{
+		public static CCLabelTtf PCLLabel(string message)
+		{
+			var label = new CCLabelTtf(message, "MarkerFelt", 22);
+			label.Color = CCColor3B.White;
+			label.Position = CCDrawManager.VisibleSize.Center;
+			return label;
+		}
+	}
+}
+
