a0ae21be8c964a85c40118a91838aa8c049971ba
Rami Tabbara <rtabbara@gmail.com>
2014-05-12 11:34:57 +1000

    Cleanup of CCControlSaturationPicker:
    * replacing ivars with auto-imp properties where possible
    * removing private InitWith.. method
    * renaming of event handling methods to begin with uppercase

--- src/extentions/GUI/CCControlExtension/CCControlSaturationPicker.cs
+++ src/extentions/GUI/CCControlExtension/CCControlSaturationPicker.cs
@@ -7,58 +7,21 @@ namespace CocosSharp
         /** Contains the receiver's current saturation value. */
 
         //not sure if these need to be there actually. I suppose someone might want to access the sprite?
-        private CCSprite _background;
-        private float _brightness;
-        private CCSprite _overlay;
-        private float _saturation;
-        private CCSprite _shadow;
-        private CCSprite _slider;
-        private CCPoint _startPos;
+
         protected int boxPos;
         protected int boxSize;
 
 
-        public float Saturation
-        {
-            get { return _saturation; }
-            set { _saturation = value; }
-        }
+		#region Properties
 
-        public float Brightness
-        {
-            get { return _brightness; }
-            set { _brightness = value; }
-        }
+		public float Saturation { get; set; }
+		public float Brightness { get; set; }
+		public CCPoint StartPos { get; set; }
 
-        public CCSprite Background
-        {
-            get { return _background; }
-            set { _background = value; }
-        }
-
-        public CCSprite Overlay
-        {
-            get { return _overlay; }
-            set { _overlay = value; }
-        }
-
-        public CCSprite Shadow
-        {
-            get { return _shadow; }
-            set { _shadow = value; }
-        }
-
-        public CCSprite Slider
-        {
-            get { return _slider; }
-            set { _slider = value; }
-        }
-
-        public CCPoint StartPos
-        {
-            get { return _startPos; }
-            set { _startPos = value; }
-        }
+		public CCSprite Background { get; set; }
+		public CCSprite Overlay { get; set; }
+		public CCSprite Shadow { get; set; }
+		public CCSprite Slider { get; set; }
 
         public override bool Enabled
         {
@@ -66,46 +29,37 @@ namespace CocosSharp
             set
             {
                 base.Enabled = value;
-                if (_slider != null)
+                if (Slider != null)
                 {
-                    _slider.Opacity = value ? (byte) 255 : (byte) 128;
+                    Slider.Opacity = value ? (byte) 255 : (byte) 128;
                 }
             }
         }
 
+		#endregion Properties
+
 
         #region Constructors
 
-        public CCControlSaturationBrightnessPicker()
-        {
-        }
-
         public CCControlSaturationBrightnessPicker(CCNode target, CCPoint pos)
         {
-            InitCCControlSaturationBrightnessPicker(target, pos);
-        }
+			Background = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerBackground.png", target, pos, CCPoint.Zero);
+			Overlay = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerOverlay.png", target, pos, CCPoint.Zero);
+			Shadow = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerShadow.png", target, pos, CCPoint.Zero);
+			Slider = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPicker.png", target, pos, new CCPoint(0.5f, 0.5f));
 
-        private void InitCCControlSaturationBrightnessPicker(CCNode target, CCPoint pos)
-        {
+			StartPos = pos;        
+			boxPos = 35; 										// starting position of the virtual box area for picking a colour
+			boxSize = (int) Background.ContentSize.Width / 2; 	// the size (width and height) of the virtual box for picking a colour from
+
 			// Register Touch Event
 			var touchListener = new CCEventListenerTouchOneByOne();
 			touchListener.IsSwallowTouches = true;
 
-			touchListener.OnTouchBegan = onTouchBegan;
-			touchListener.OnTouchMoved = onTouchMoved;
+			touchListener.OnTouchBegan = OnTouchBegan;
+			touchListener.OnTouchMoved = OnTouchMoved;
 
 			EventDispatcher.AddEventListener(touchListener, this);
-
-            // Add background and slider sprites
-            _background = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerBackground.png", target, pos, CCPoint.Zero);
-            _overlay = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerOverlay.png", target, pos, CCPoint.Zero);
-            _shadow = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPickerShadow.png", target, pos, CCPoint.Zero);
-            _slider = CCControlUtils.AddSpriteToTargetWithPosAndAnchor("colourPicker.png", target, pos, new CCPoint(0.5f, 0.5f));
-
-            _startPos = pos; // starting position of the colour picker        
-            boxPos = 35; // starting position of the virtual box area for picking a colour
-            boxSize = (int) _background.ContentSize.Width / 2;
-            // the size (width and height) of the virtual box for picking a colour from
         }
 
         #endregion Constructors
@@ -119,17 +73,14 @@ namespace CocosSharp
             hsvTemp.v = 1;
 
             RGBA rgb = CCControlUtils.RGBfromHSV(hsvTemp);
-            _background.Color = new CCColor3B((byte) (rgb.r * 255.0f), (byte) (rgb.g * 255.0f),
-                                              (byte) (rgb.b * 255.0f));
+			Background.Color = new CCColor3B((byte) (rgb.r * 255.0f), (byte) (rgb.g * 255.0f),  (byte) (rgb.b * 255.0f));
         }
 
         public virtual void UpdateDraggerWithHSV(HSV hsv)
         {
             // Set the position of the slider to the correct saturation and brightness
-            var pos = new CCPoint(_startPos.X + boxPos + (boxSize * (1f - hsv.s)),
-                                  _startPos.Y + boxPos + (boxSize * hsv.v));
+			var pos = new CCPoint(StartPos.X + boxPos + (boxSize * (1f - hsv.s)), StartPos.Y + boxPos + (boxSize * hsv.v));
 
-            // update
             UpdateSliderPosition(pos);
         }
 
@@ -137,9 +88,8 @@ namespace CocosSharp
         {
             // Clamp the position of the icon within the circle
 
-            // Get the center point of the bkgd image
-            float centerX = _startPos.X + _background.BoundingBox.Size.Width * 0.5f;
-            float centerY = _startPos.Y + _background.BoundingBox.Size.Height * 0.5f;
+            float centerX = StartPos.X + Background.BoundingBox.Size.Width * 0.5f;
+            float centerY = StartPos.Y + Background.BoundingBox.Size.Height * 0.5f;
 
             // Work out the distance difference between the location and center
             float dx = sliderPosition.X - centerX;
@@ -150,47 +100,42 @@ namespace CocosSharp
             var angle = (float) Math.Atan2(dy, dx);
 
             // Set the limit to the slider movement within the colour picker
-            float limit = _background.BoundingBox.Size.Width * 0.5f;
+            float limit = Background.BoundingBox.Size.Width * 0.5f;
 
-            // Check distance doesn't exceed the bounds of the circle
             if (dist > limit)
             {
                 sliderPosition.X = centerX + limit * (float) Math.Cos(angle);
                 sliderPosition.Y = centerY + limit * (float) Math.Sin(angle);
             }
 
-            // Set the position of the dragger
-            _slider.Position = sliderPosition;
-
+            Slider.Position = sliderPosition;
 
             // Clamp the position within the virtual box for colour selection
-            if (sliderPosition.X < _startPos.X + boxPos) sliderPosition.X = _startPos.X + boxPos;
-            else if (sliderPosition.X > _startPos.X + boxPos + boxSize - 1)
-                sliderPosition.X = _startPos.X + boxPos + boxSize - 1;
-            if (sliderPosition.Y < _startPos.Y + boxPos) sliderPosition.Y = _startPos.Y + boxPos;
-            else if (sliderPosition.Y > _startPos.Y + boxPos + boxSize)
-                sliderPosition.Y = _startPos.Y + boxPos + boxSize;
+            if (sliderPosition.X < StartPos.X + boxPos) sliderPosition.X = StartPos.X + boxPos;
+            else if (sliderPosition.X > StartPos.X + boxPos + boxSize - 1)
+                sliderPosition.X = StartPos.X + boxPos + boxSize - 1;
+            if (sliderPosition.Y < StartPos.Y + boxPos) sliderPosition.Y = StartPos.Y + boxPos;
+            else if (sliderPosition.Y > StartPos.Y + boxPos + boxSize)
+                sliderPosition.Y = StartPos.Y + boxPos + boxSize;
 
             // Use the position / slider width to determin the percentage the dragger is at
-            _saturation = 1.0f - Math.Abs((_startPos.X + boxPos - sliderPosition.X) / boxSize);
-            _brightness = Math.Abs((_startPos.Y + boxPos - sliderPosition.Y) / boxSize);
+            Saturation = 1.0f - Math.Abs((StartPos.X + boxPos - sliderPosition.X) / boxSize);
+            Brightness = Math.Abs((StartPos.Y + boxPos - sliderPosition.Y) / boxSize);
         }
 
         protected bool CheckSliderPosition(CCPoint location)
         {
             // Clamp the position of the icon within the circle
 
-            // get the center point of the bkgd image
-            float centerX = _startPos.X + _background.BoundingBox.Size.Width * 0.5f;
-            float centerY = _startPos.Y + _background.BoundingBox.Size.Height * 0.5f;
+            float centerX = StartPos.X + Background.BoundingBox.Size.Width * 0.5f;
+            float centerY = StartPos.Y + Background.BoundingBox.Size.Height * 0.5f;
 
-            // work out the distance difference between the location and center
             float dx = location.X - centerX;
             float dy = location.Y - centerY;
             var dist = (float) Math.Sqrt(dx * dx + dy * dy);
 
             // check that the touch location is within the bounding rectangle before sending updates
-            if (dist <= _background.BoundingBox.Size.Width * 0.5f)
+            if (dist <= Background.BoundingBox.Size.Width * 0.5f)
             {
                 UpdateSliderPosition(location);
                 SendActionsForControlEvents(CCControlEvent.ValueChanged);
@@ -199,8 +144,9 @@ namespace CocosSharp
             return false;
         }
 
+		#region Event handling
 
-		bool onTouchBegan(CCTouch touch, CCEvent touchEvent)
+		bool OnTouchBegan(CCTouch touch, CCEvent touchEvent)
         {
             if (!Enabled || !Visible)
             {
@@ -214,7 +160,7 @@ namespace CocosSharp
             return CheckSliderPosition(touchLocation);
         }
 
-		void onTouchMoved(CCTouch touch, CCEvent touchEvent)
+		void OnTouchMoved(CCTouch touch, CCEvent touchEvent)
         {
             // Get the touch location
             CCPoint touchLocation = GetTouchLocation(touch);
@@ -225,5 +171,7 @@ namespace CocosSharp
             // Check the touch position on the slider
             CheckSliderPosition(touchLocation);
         }
+
+		#endregion Event handling
     }
 }
