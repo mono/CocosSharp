8a90440aa59fddd3c43f958f55729649897c2743
Rami Tabbara <rtabbara@gmail.com>
2014-05-27 13:33:27 +1000

    Removing reference to Linq in a few small classes:
    * zip_support
    * text_input_node

--- src/text_input_node/CCIMEKeyboardNotificationInfo.cs
+++ src/text_input_node/CCIMEKeyboardNotificationInfo.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace CocosSharp
 {
     public class CCIMEKeyboardNotificationInfo
     {
-        public CCRect begin;              // the soft keyboard rectangle when animatin begin
-        public CCRect end;                // the soft keyboard rectangle when animatin end
-        public float duration;           // the soft keyboard animation duration
+		public CCRect Begin;              	// the soft keyboard rectangle when animatin begin
+		public CCRect End;                	// the soft keyboard rectangle when animatin end
+		public float Duration;           	// the soft keyboard animation duration
     }
 }
