ed6555dfcf910f4971252e383963b4e0f61e2524
Rami Tabbara <rtabbara@gmail.com>
2014-05-27 12:38:15 +1000

    Cleanup of CConfiguration:
    * remove reference to Linq
    * replace ivars with properies

--- src/CCConfiguration.cs
+++ src/CCConfiguration.cs
@@ -24,12 +24,13 @@ THE SOFTWARE.
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace CocosSharp
 {
-    public enum CCGlesVersion
+	#region Enums
+
+	public enum CCGlesVersion
     {
         GLES_VER_INVALID,
         GLES_VER_1_0,
@@ -37,115 +38,60 @@ namespace CocosSharp
         GLES_VER_2_0,
     }
 
+	#endregion Enums
+
+
     public class CCConfiguration
     {
-        protected int m_nMaxTextureSize = 0x0D33;
-        protected int m_nMaxModelviewStackDepth;
-        protected bool m_bSupportsPVRTC;
-        protected bool m_bSupportsNPOT;
-        protected bool m_bSupportsBGRA8888;
-        protected bool m_bSupportsDiscardFramebuffer;
-        protected bool m_bInited;
-        protected uint m_uOSVersion;
-        protected int m_nMaxSamplesAllowed;
-        protected string m_pGlExtensions;
+		static CCConfiguration sharedConfiguration = new CCConfiguration();
 
-        private CCConfiguration()
-        { }
+		#region Properties
 
-        public CCGlesVersion getGlesVersion()
+		// Returns a shared instance of the CCConfiguration
+		public static CCConfiguration SharedConfiguration
+		{
+			get 
+			{
+				if (!sharedConfiguration.Inited) 
+				{
+					sharedConfiguration.Inited = true;
+				}
+
+				return sharedConfiguration;
+			}
+		}
+
+		public int MaxTextureSize { get; private set; } 				// OpenGL Max texture size.
+		public int MaxModelviewStackDepth { get; private set; } 		// OpenGL Max Modelview Stack Depth
+		public bool IsSupportsNPOT { get; private set; }         		// Whether or not the GPU supports NPOT (Non Power Of Two) textures.
+		public bool IsSupportsPVRTC { get; private set; } 				// Whether or not PVR Texture Compressed is supported
+		public bool IsSupportsBGRA8888 { get; private set; }        	// Whether or not BGRA8888 textures are supported.
+		public bool IsSupportsDiscardFramebuffer { get; private set; }	// Whether or not glDiscardFramebufferEXT is supported
+		public uint OSVersion { get; private set; }       				// Returns the OS version. iOS = firmware version. Mac = OS version
+		protected bool Inited { get; private set; }
+
+        public CCGlesVersion GlesVersion
         {
-            return CCGlesVersion.GLES_VER_2_0;
+			get { return CCGlesVersion.GLES_VER_2_0; }
         }
 
-        /// <summary>
-        /// OpenGL Max texture size.
-        /// </summary>
-        public int MaxTextureSize
-        {
-            get { return m_nMaxTextureSize; }
-        }
-
-        /// <summary>
-        /// OpenGL Max Modelview Stack Depth
-        /// </summary>
-        public int MaxModelviewStackDepth
-        {
-            get { return m_nMaxModelviewStackDepth; }
-        }
-
-        /// <summary>
-        /// Whether or not the GPU supports NPOT (Non Power Of Two) textures.
-        /// NPOT textures have the following limitations:
-        ///- They can't have mipmaps
-        ///- They only accept GL_CLAMP_TO_EDGE in GL_TEXTURE_WRAP_{S,T}
-        /// @since v0.99.2
-        /// </summary>
-        public bool IsSupportsNPOT
-        {
-            get { return m_bSupportsNPOT; }
-        }
-
-        /// <summary>
-        /// Whether or not PVR Texture Compressed is supported
-        /// </summary>
-        public bool IsSupportsPVRTC
-        {
-            get { return m_bSupportsPVRTC; }
-        }
-
-        /// <summary>
-        /// Whether or not BGRA8888 textures are supported.
-        /// @since v0.99.2
-        /// </summary>
-        public bool IsSupportsBGRA8888
-        {
-            get { return m_bSupportsBGRA8888; }
-        }
-
-        /// <summary>
-        /// Whether or not glDiscardFramebufferEXT is supported
-        /// @since v0.99.2
-        /// </summary>
-        public bool IsSupportsDiscardFramebuffer
-        {
-            get { return m_bSupportsDiscardFramebuffer; }
-        }
-
-        /// <summary>
-        /// returns the OS version.
-        ///  - On iOS devices it returns the firmware version.
-        /// - On Mac returns the OS version
-        /// @since v0.99.5
-        /// </summary>
-        public uint OSVersion
-        {
-            get { return m_uOSVersion; }
-        }
-
-        /// <summary>
-        /// returns whether or not an OpenGL is supported
-        /// </summary>
+		// Returns whether or not an OpenGL is supported
         public bool CheckForGLExtension(string searchName)
         {
             throw new NotImplementedException();
         }
 
-        static CCConfiguration m_sharedConfiguration = new CCConfiguration();
+		#endregion Properties
 
-        /// <summary>
-        /// returns a shared instance of the CCConfiguration
-        /// </summary>
-        public static CCConfiguration SharedConfiguration
-        {
-            get {
-            if (!m_sharedConfiguration.m_bInited)
-            {
-                m_sharedConfiguration.m_bInited = true;
-            }
 
-            return m_sharedConfiguration;
-        }
+		#region Constructors
+
+		CCConfiguration()
+		{ 
+			MaxTextureSize = 0x0D33;
+		}
+
+		#endregion Constructors
+
     }
-}
 }
