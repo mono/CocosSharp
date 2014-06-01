ab6a80f26568258b4a636aa8227074de559e788d
kjpou1 <kjpou@pt.lu>
2014-06-01 10:32:57 +0200

    Remove Debug.Listeners.Add from the Mac version.  This duplicates the output and is no longer necessary.

--- tests/tests/Program.cs
+++ tests/tests/Program.cs
@@ -60,13 +60,6 @@ namespace tests
 
 		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
 		{
-#if DEBUG
-			/* Create a listener that outputs to the console screen, and 
-  			* add it to the debug listeners. */
-			TextWriterTraceListener debugConsoleWriter = new 
-				TextWriterTraceListener(System.Console.Out);
-			Debug.Listeners.Add(debugConsoleWriter);
-#endif
 			// Fun begins..
 			game = new Game1();
 			game.Run();
