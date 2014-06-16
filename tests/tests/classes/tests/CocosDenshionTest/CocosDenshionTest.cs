c356742b4191927cf58ce381cfebb5de065095c7
Rami Tabbara <rtabbara@gmail.com>
2014-06-16 11:45:30 +1000

    Cleanup of CocosDenshionTest:
    * removing calls to MainWindowDirector
    * renaming ivars/methods
    * fixing indentation

--- tests/tests/classes/tests/CocosDenshionTest/CocosDenshionTest.cs
+++ tests/tests/classes/tests/CocosDenshionTest/CocosDenshionTest.cs
@@ -11,215 +11,229 @@ namespace tests
 {
     public class CocosDenshionTest : CCLayer
     {
-        string EFFECT_FILE = "Sounds/effect1";
-        string MUSIC_FILE = "Sounds/background";
-        int LINE_SPACE = 40;
+        static readonly string EFFECT_FILE = "Sounds/effect1";
+        static readonly string MUSIC_FILE = "Sounds/background";
+        const int LINE_SPACE = 40;
 
-        CCMenu m_pItmeMenu;
-	    CCPoint m_tBeginPos;
-	    int m_nTestCount;
-	    int m_nSoundId;
+        CCMenu testMenu;
+        List<CCMenuItem> testMenuItems;
+        CCPoint beginPos;
+        int soundId;
+
+
+        #region Constructors
 
         public CocosDenshionTest()
         {
-            m_pItmeMenu = null;
-            m_tBeginPos = new CCPoint(0,0);
-            m_nSoundId = 0;
+            testMenu = null;
+            beginPos = new CCPoint(0,0);
+            soundId = 0;
 
-	        string[] testItems = {
-		        "play background music",
-		        "stop background music",
-		        "pause background music",
-		        "resume background music",
-		        "rewind background music",
-		        "is background music playing",
-		        "play effect",
-                "play effect repeatly",
-		        "stop effect",
-		        "unload effect",
-		        "add background music volume",
-		        "sub background music volume",
-		        "add effects volume",
-		        "sub effects volume"
-	        };
+            string[] testItems = {
+                "Play background music",
+                "Stop background music",
+                "Pause background music",
+                "Resume background music",
+                "Rewind background music",
+                "Is background music playing",
+                "Play effect",
+                "Play effect repeatly",
+                "Stop effect",
+                "Unload effect",
+                "Add background music volume",
+                "Sub background music volume",
+                "Add effects volume",
+                "Sub effects volume"
+            };
 
-	        // add menu items for tests
-	        m_pItmeMenu = new CCMenu(null);
-	        CCSize s = CCApplication.SharedApplication.MainWindowDirector.WindowSizeInPoints;
-	        m_nTestCount = testItems.Count<string>();
+            testMenu = new CCMenu(null);
+            testMenuItems = new List<CCMenuItem>();
 
-	        for (int i = 0; i < m_nTestCount; ++i)
-	        {
+            for(int i=0; i < testItems.Count(); ++i)
+            {
                 CCLabelTtf label = new CCLabelTtf(testItems[i], "arial", 24);
-                CCMenuItemLabelTTF pMenuItem = new CCMenuItemLabelTTF(label, menuCallback);
-		
-		        m_pItmeMenu.AddChild(pMenuItem, i + 10000);
-		        pMenuItem.Position = new CCPoint( s.Width / 2, (s.Height - (i + 1) * LINE_SPACE) );
-	        }
+                CCMenuItemLabelTTF menuItem = new CCMenuItemLabelTTF(label, MenuCallback);
+                testMenu.AddChild(menuItem, i + 10000);
+                testMenuItems.Add(menuItem);
+            }
 
-	        m_pItmeMenu.ContentSize = new CCSize(s.Width, (m_nTestCount + 1) * LINE_SPACE);
-	        m_pItmeMenu.Position = new CCPoint(0,0);
-	        AddChild(m_pItmeMenu);
+            AddChild(testMenu);
 
-			// Register Touch Event
-			var touchListener = new CCEventListenerTouchAllAtOnce();
+            // preload background music and effect
+            CCSimpleAudioEngine.SharedEngine.PreloadBackgroundMusic(CCFileUtils.FullPathFromRelativePath(MUSIC_FILE));
+            CCSimpleAudioEngine.SharedEngine.PreloadEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
 
-			touchListener.OnTouchesBegan = onTouchesBegan;
-			touchListener.OnTouchesMoved = onTouchesMoved;
-
-			EventDispatcher.AddEventListener(touchListener, this);   
-
-	        // preload background music and effect
-	        CCSimpleAudioEngine.SharedEngine.PreloadBackgroundMusic(CCFileUtils.FullPathFromRelativePath(MUSIC_FILE));
-	        CCSimpleAudioEngine.SharedEngine.PreloadEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
-    
             // set default volume
             CCSimpleAudioEngine.SharedEngine.EffectsVolume = 0.5f;
             CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume = 0.5f;
         }
 
-        ~CocosDenshionTest()
+        #endregion Constructors
+
+
+        #region Setup content
+
+        protected override void RunningOnNewWindow(CCSize windowSize)
         {
+            base.RunningOnNewWindow(windowSize);
+
+            // Layout content
+            int testCount = testMenuItems.Count();
+
+            testMenu.ContentSize = new CCSize(windowSize.Width, (testCount + 1) * LINE_SPACE);
+            testMenu.Position = new CCPoint(0,0);
+
+            for(int i=0; i < testCount; ++i)
+            {
+                testMenuItems[i].Position 
+                    = new CCPoint( windowSize.Width / 2, (windowSize.Height - (i + 1) * LINE_SPACE));
+            }
+
+            // Register touch events
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+
+            touchListener.OnTouchesBegan = OnTouchesBegan;
+            touchListener.OnTouchesMoved = OnTouchesMoved;
+
+            EventDispatcher.AddEventListener(touchListener, this);
         }
+
+        #endregion Setup content
+
 
         public override void OnExit()
         {
-	        base.OnExit();
+            base.OnExit();
 
-	        CCSimpleAudioEngine.SharedEngine.End();
+            CCSimpleAudioEngine.SharedEngine.End();
         }
 
-        public void menuCallback(object pSender)
+        public void MenuCallback(object sender)
         {
-	        // get the userdata, it's the index of the menu item clicked
-	        CCMenuItem pMenuItem = (CCMenuItem)(pSender);
-	        int nIdx = pMenuItem.ZOrder - 10000;
+            // get the userdata, it's the index of the menu item clicked
+            CCMenuItem menuItem = (CCMenuItem)(sender);
+            int nIdx = menuItem.ZOrder - 10000;
 
-	        switch(nIdx)
-	        {
-	        // play background music
-	        case 0:
+            switch(nIdx)
+            {
+                // play background music
+                case 0:
+                    CCSimpleAudioEngine.SharedEngine.PlayBackgroundMusic(CCFileUtils.FullPathFromRelativePath(MUSIC_FILE), true);
+                    break;
+                // stop background music
+                case 1:
+                    CCSimpleAudioEngine.SharedEngine.StopBackgroundMusic();
+                    break;
+                // pause background music
+                case 2:
+                    CCSimpleAudioEngine.SharedEngine.PauseBackgroundMusic();
+                    break;
+                // resume background music
+                case 3:
+                    CCSimpleAudioEngine.SharedEngine.ResumeBackgroundMusic();
+                    break;
+                // rewind background music
+                case 4:
+                    CCSimpleAudioEngine.SharedEngine.RewindBackgroundMusic();
+                    break;
+                // is background music playing
+                case 5:
+                    if(CCSimpleAudioEngine.SharedEngine.BackgroundMusicPlaying)
+                    {
+                        CCLog.Log("background music is playing");
+                    }
+                    else
+                    {
+                        CCLog.Log("background music is not playing");
+                    }
+                    break;
+                // play effect
+                case 6:
+                    soundId = CCSimpleAudioEngine.SharedEngine.PlayEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
+                    break;
+                // play effect
+                case 7:
+                    soundId = CCSimpleAudioEngine.SharedEngine.PlayEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE), true);
+                    break;
+                // stop effect
+                case 8:
+                    CCSimpleAudioEngine.SharedEngine.StopEffect(soundId);
+                    break;
+                // unload effect
+                case 9:
+                    CCSimpleAudioEngine.SharedEngine.UnloadEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
+                    break;
+                // add bakcground music volume
+                case 10:
+                    CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume = CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume + 0.1f;
+                    break;
+                // sub backgroud music volume
+                case 11:
+                    CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume = CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume - 0.1f;
+                    break;
+                // add effects volume
+                case 12:
+                    CCSimpleAudioEngine.SharedEngine.EffectsVolume = CCSimpleAudioEngine.SharedEngine.EffectsVolume + 0.1f;
+                    break;
+                // sub effects volume
+                case 13:
+                    CCSimpleAudioEngine.SharedEngine.EffectsVolume = CCSimpleAudioEngine.SharedEngine.EffectsVolume - 0.1f;
+                    break;
+            }
 
-		        CCSimpleAudioEngine.SharedEngine.PlayBackgroundMusic(CCFileUtils.FullPathFromRelativePath(MUSIC_FILE), true);
-		        break;
-	        // stop background music
-	        case 1:
-		        CCSimpleAudioEngine.SharedEngine.StopBackgroundMusic();
-		        break;
-	        // pause background music
-	        case 2:
-		        CCSimpleAudioEngine.SharedEngine.PauseBackgroundMusic();
-		        break;
-	        // resume background music
-	        case 3:
-		        CCSimpleAudioEngine.SharedEngine.ResumeBackgroundMusic();
-		        break;
-	        // rewind background music
-	        case 4:
-		        CCSimpleAudioEngine.SharedEngine.RewindBackgroundMusic();
-		        break;
-	        // is background music playing
-	        case 5:
-		        if (CCSimpleAudioEngine.SharedEngine.BackgroundMusicPlaying)
-		        {
-			        CCLog.Log("background music is playing");
-		        }
-		        else
-		        {
-                    CCLog.Log("background music is not playing");
-		        }
-		        break;
-	        // play effect
-	        case 6:
-		        m_nSoundId = CCSimpleAudioEngine.SharedEngine.PlayEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
-		        break;
-            // play effect
-            case 7:
-                m_nSoundId = CCSimpleAudioEngine.SharedEngine.PlayEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE), true);
-                break;
-            // stop effect
-	        case 8:
-		        CCSimpleAudioEngine.SharedEngine.StopEffect(m_nSoundId);
-		        break;
-	        // unload effect
-	        case 9:
-		        CCSimpleAudioEngine.SharedEngine.UnloadEffect(CCFileUtils.FullPathFromRelativePath(EFFECT_FILE));
-		        break;
-		        // add bakcground music volume
-	        case 10:
-		        CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume = CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume + 0.1f;
-		        break;
-		        // sub backgroud music volume
-	        case 11:
-		        CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume = CCSimpleAudioEngine.SharedEngine.BackgroundMusicVolume - 0.1f;
-		        break;
-		        // add effects volume
-	        case 12:
-		        CCSimpleAudioEngine.SharedEngine.EffectsVolume = CCSimpleAudioEngine.SharedEngine.EffectsVolume + 0.1f;
-		        break;
-		        // sub effects volume
-	        case 13:
-		        CCSimpleAudioEngine.SharedEngine.EffectsVolume = CCSimpleAudioEngine.SharedEngine.EffectsVolume - 0.1f;
-		        break;
-	        }
-	
         }
 
-		void onTouchesBegan(List<CCTouch> touches, CCEvent touchEvent)
+        #region Event handling
+
+        void OnTouchesBegan(List<CCTouch> touches, CCEvent touchEvent)
         {
-			CCTouch touch = touches.FirstOrDefault();
+            CCTouch touch = touches.FirstOrDefault();
 
-            var m_tBeginPos = touch.Location;
+            var beginPos = touch.Location;
         }
 
-		void onTouchesMoved(List<CCTouch> touches, CCEvent touchEvent)
+        void OnTouchesMoved(List<CCTouch> touches, CCEvent touchEvent)
         {
-			CCTouch touch = touches.FirstOrDefault();
+            CCTouch touch = touches.FirstOrDefault();
 
-	        CCPoint touchLocation = touch.LocationInView;	
-	        touchLocation = CCApplication.SharedApplication.MainWindowDirector.ConvertToGl( touchLocation );
-	        float nMoveY = touchLocation.Y - m_tBeginPos.Y;
+            CCPoint touchLocation = touch.LocationInView;   
+            touchLocation = Director.ConvertToGl( touchLocation );
+            float nMoveY = touchLocation.Y - beginPos.Y;
 
-	        CCPoint curPos  = m_pItmeMenu.Position;
-	        CCPoint nextPos = new CCPoint(curPos.X, curPos.Y + nMoveY);
-	        CCSize winSize = CCApplication.SharedApplication.MainWindowDirector.WindowSizeInPoints;
-	        if (nextPos.Y < 0.0f)
-	        {
-		        m_pItmeMenu.Position = new CCPoint(0,0);
-		        return;
-	        }
+            CCPoint curPos  = testMenu.Position;
+            CCPoint nextPos = new CCPoint(curPos.X, curPos.Y + nMoveY);
+            CCSize winSize = Director.WindowSizeInPoints;
 
-	        if (nextPos.Y > ((m_nTestCount + 1)* LINE_SPACE - winSize.Height))
-	        {
-		        m_pItmeMenu.Position = new CCPoint(0, ((m_nTestCount + 1)* LINE_SPACE - winSize.Height));
-		        return;
-	        }
+            if (nextPos.Y < 0.0f)
+            {
+                testMenu.Position = new CCPoint(0,0);
+                return;
+            }
 
-	        m_pItmeMenu.Position = nextPos;
-	        m_tBeginPos = touchLocation;
+            int testCount = testMenuItems.Count();
+
+            if (nextPos.Y > ((testCount + 1)* LINE_SPACE - winSize.Height))
+            {
+                testMenu.Position = new CCPoint(0, ((testCount + 1)* LINE_SPACE - winSize.Height));
+                return;
+            }
+
+            testMenu.Position = nextPos;
+            beginPos = touchLocation;
         }
 
-
+        #endregion Event handling
     }
 
 
     public class CocosDenshionTestScene : TestScene
     {
-        protected override void NextTestCase()
-        {
-        }
-        protected override void PreviousTestCase()
-        {
-        }
-        protected override void RestTestCase()
-        {
-        }
         public override void runThisTest()
         {
-	        CCLayer pLayer = new CocosDenshionTest();
-	        AddChild(pLayer);
+            CCLayer layer = new CocosDenshionTest();
+            AddChild(layer);
 
-	        CCApplication.SharedApplication.MainWindowDirector.ReplaceScene(this);
+            Director.ReplaceScene(this);
         }
     }
 }
