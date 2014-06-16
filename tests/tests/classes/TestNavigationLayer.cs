abb17b8cfad703f483b3e01eeb5596d6e9fc0885
Rami Tabbara <rtabbara@gmail.com>
2014-06-16 12:55:13 +1000

    Added TestNavigationLayer:
    * a lot of test cases have a navigation menu to move between test cases
    * reduce repeated code by having this abstract class

---
+++ tests/tests/classes/TestNavigationLayer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using CocosSharp;
+
+namespace tests
+{
+    // Subclass this for test cases with a back/restart/next navigation bar as well as a title/subtitle labels
+    public abstract class TestNavigationLayer : CCLayer
+    {
+        CCMenu navigationMenu;
+        CCMenuItem backMenuItem;
+        CCMenuItem restartMenuItem;
+        CCMenuItem nextMenuItem;
+
+
+        #region Properties
+
+        protected CCLabelTtf TitleLabel { get; private set; }
+        protected CCLabelTtf SubtitleLabel { get; private set; }
+
+        public virtual string Title
+        {
+            get { return ""; }
+        }
+
+        public virtual string Subtitle
+        {
+            get { return ""; }
+        }
+
+        #endregion Properties
+
+
+        #region Constructors
+
+        public TestNavigationLayer()
+        {
+            TitleLabel = new CCLabelTtf(Title, "arial", 32);
+            AddChild(TitleLabel, 1);
+
+            string subtitleStr = Subtitle;
+            if (subtitleStr.Length > 0)
+            {
+                SubtitleLabel = new CCLabelTtf(subtitleStr, "arial", 16);
+                AddChild(SubtitleLabel, 1);
+            }
+
+            backMenuItem = new CCMenuItemImage(TestResource.s_pPathB1, TestResource.s_pPathB2, BackCallback);
+            restartMenuItem = new CCMenuItemImage(TestResource.s_pPathR1, TestResource.s_pPathR2, RestartCallback);
+            nextMenuItem = new CCMenuItemImage(TestResource.s_pPathF1, TestResource.s_pPathF2, NextCallback);
+
+            navigationMenu = new CCMenu(backMenuItem, restartMenuItem, nextMenuItem);
+
+            AddChild(navigationMenu, 1);
+        }
+
+        #endregion Constructors
+
+
+        #region Setup content
+
+        protected override void RunningOnNewWindow(CCSize windowSize)
+        {
+            base.RunningOnNewWindow(windowSize);
+
+            if(TitleLabel != null)
+                TitleLabel.Position = (new CCPoint(windowSize.Width / 2, windowSize.Height - 50));
+
+            if(SubtitleLabel != null)
+                SubtitleLabel.Position = (new CCPoint(windowSize.Width / 2, windowSize.Height - 80));
+
+            navigationMenu.Position = new CCPoint(0, 0);
+            backMenuItem.Position = new CCPoint(windowSize.Width / 2 - 100, 30);
+            restartMenuItem.Position = new CCPoint(windowSize.Width / 2, 30);
+            nextMenuItem.Position = new CCPoint(windowSize.Width / 2 + 100, 30);
+        }
+
+        #endregion Setup content
+
+
+        #region Callbacks
+
+        public virtual void RestartCallback(object sender)
+        {
+        }
+
+        public virtual void NextCallback(object sender)
+        {
+        }
+
+        public virtual void BackCallback(object sender)
+        {
+        }
+
+        #endregion Callbacks
+    }
+}
+
