6d6b96ba54d94967441b4214faca511abe2cf4a3
Rami Tabbara <rtabbara@gmail.com>
2014-07-10 15:31:51 +1000

    Adding CCViewport/CCWindow classes

---
+++ src/CCViewport.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CocosSharp
+{
+    #region Enums
+
+    public enum CCViewportResolutionPolicy
+    {
+        // The entire application is visible in the specified area without trying to preserve the original aspect ratio. 
+        // Distortion can occur, and the application may appear stretched or compressed.
+        ExactFit,
+
+        // The entire application fills the specified area, without distortion but possibly with some cropping, 
+        // while maintaining the original aspect ratio of the application.
+        AspectFill,
+
+        // The entire application is visible in the specified area without distortion while maintaining the original 
+        // aspect ratio of the application. Borders can appear on two sides of the application.
+        AspectFit
+    }
+
+    #endregion Enums
+
+
+    public class CCViewport
+    {
+        internal event EventHandler OnViewportChanged;
+
+        CCViewportResolutionPolicy resolutionPolicy;
+        CCDisplayOrientation displayOrientation;
+
+        CCRect exactFitLandscapeRatio;
+        CCRect exactFitPortraitRatio;
+        CCRect viewportInPixels;
+
+        CCSize landscapeScreenSizeInPixels;
+
+        Viewport xnaViewport;
+
+
+        #region Properties
+
+        public CCViewportResolutionPolicy ResolutionPolicy 
+        { 
+            get { return resolutionPolicy; } 
+            set 
+            {
+                if (resolutionPolicy != value) 
+                {
+                    resolutionPolicy = value;
+                    UpdateViewport();
+                }
+            }
+        }
+
+        public CCRect ExactFitLandscapeRatio
+        {
+            get { return exactFitLandscapeRatio; }
+            set 
+            {
+                if(exactFitLandscapeRatio != value) 
+                {
+                    // Ratio can only be between 0.0 and 1.0
+                    value.Origin.X = CCMathHelper.Clamp(value.Origin.X, 0.0f, 1.0f);
+                    value.Origin.Y = CCMathHelper.Clamp(value.Origin.Y, 0.0f, 1.0f);
+                    value.Size.Width = CCMathHelper.Clamp(value.Size.Width, 0.0f, 1.0f);
+                    value.Size.Height = CCMathHelper.Clamp(value.Size.Height, 0.0f, 1.0f);
+
+                    exactFitLandscapeRatio = value;
+                    UpdateViewport();
+                }
+            }
+        }
+
+        public CCRect ExactFitPortraitRatio
+        {
+            get { return exactFitPortraitRatio; }
+            set 
+            {
+                if(exactFitPortraitRatio != value) 
+                {
+                    // Ratio can only be between 0.0 and 1.0
+                    value.Origin.X = CCMathHelper.Clamp(value.Origin.X, 0.0f, 1.0f);
+                    value.Origin.Y = CCMathHelper.Clamp(value.Origin.Y, 0.0f, 1.0f);
+                    value.Size.Width = CCMathHelper.Clamp(value.Size.Width, 0.0f, 1.0f);
+                    value.Size.Height = CCMathHelper.Clamp(value.Size.Height, 0.0f, 1.0f);
+
+                    exactFitPortraitRatio = value;
+                    UpdateViewport();
+                }
+            }
+        }
+
+        internal float AspectRatio
+        {
+            get { return viewportInPixels.Size.Width / viewportInPixels.Size.Height; }
+        }
+
+        internal CCRect ViewportInPixels
+        {
+            get { return viewportInPixels; }
+        }
+
+        internal CCDisplayOrientation DisplayOrientation
+        {
+            get { return displayOrientation; }
+            set 
+            {
+                if(displayOrientation != value) 
+                {
+                    displayOrientation = value;
+                    UpdateViewport();
+                }
+            }
+        }
+
+        internal CCSize LandscapeScreenSizeInPixels
+        {
+            set 
+            {
+                if(landscapeScreenSizeInPixels != value) 
+                {
+                    landscapeScreenSizeInPixels = value;
+                    UpdateViewport();
+                }
+            }
+        }
+
+        internal Viewport XnaViewport
+        {
+            get { return xnaViewport; }
+        }
+
+        #endregion Properties
+
+
+        #region Constructors
+
+        public CCViewport(
+            CCRect exactFitLandscapeRatioIn, CCRect exactFitPortraitRatioIn = default(CCRect), 
+            CCViewportResolutionPolicy resolutionPolicyIn=CCViewportResolutionPolicy.AspectFit, 
+            CCDisplayOrientation displayOrientationIn=CCDisplayOrientation.LandscapeLeft)
+        {
+            if(exactFitPortraitRatioIn == default(CCRect))
+            {
+                exactFitPortraitRatioIn = exactFitLandscapeRatioIn;
+            }
+
+            resolutionPolicy = resolutionPolicyIn;
+            displayOrientation = displayOrientationIn;
+            exactFitLandscapeRatio = exactFitLandscapeRatioIn;
+            exactFitPortraitRatio = exactFitPortraitRatioIn;
+        }
+
+        #endregion Constructors
+
+
+        void UpdateViewport()
+        {
+            if(landscapeScreenSizeInPixels == CCSize.Zero)
+                return;
+
+            bool isPortrat = DisplayOrientation.IsPortrait();
+
+            CCRect exactFitRectRatio = isPortrat ? ExactFitPortraitRatio : ExactFitLandscapeRatio;
+            CCSize screenSize = isPortrat ? landscapeScreenSizeInPixels.Inverted : landscapeScreenSizeInPixels;
+
+            Rectangle exactFitRectPixels = new Rectangle (
+                (int)(exactFitRectRatio.Origin.X * screenSize.Width), 
+                (int)(exactFitRectRatio.Origin.Y * screenSize.Height),
+                (int)(exactFitRectRatio.Size.Width * screenSize.Width),
+                (int)(exactFitRectRatio.Size.Height * screenSize.Height)
+            );
+
+            Rectangle xnaViewportRect = exactFitRectPixels;
+
+            float exactFitAspectRatio = exactFitRectPixels.Width / exactFitRectPixels.Height;
+            float screenAspectRatio = screenSize.Width / (float)screenSize.Height;
+
+            switch(ResolutionPolicy) 
+            {
+                case CCViewportResolutionPolicy.AspectFit:
+                    // The screen width > screen height    
+                    if (screenAspectRatio > 1.0f) 
+                    {
+                        xnaViewportRect.Height = (int) (xnaViewportRect.Width / screenAspectRatio);
+                    } 
+                    else 
+                    {
+                        xnaViewportRect.Width = (int) (xnaViewportRect.Height * screenAspectRatio);
+                    }
+                    break;
+                case CCViewportResolutionPolicy.AspectFill:
+                    // The exact fit viewport width > exact fit viewport height   
+                    if (exactFitAspectRatio > 1.0f) 
+                    {
+                        xnaViewportRect.Height = (int) (xnaViewportRect.Width / screenAspectRatio);
+                    } 
+                    else 
+                    {
+                        xnaViewportRect.Width = (int) (xnaViewportRect.Height * screenAspectRatio);
+                    }    
+                    break;
+                    // Defaults to exact fit
+                default:
+                    break;
+            }
+
+            xnaViewportRect.X = (int)Math.Floor((exactFitRectPixels.Width - xnaViewportRect.Width) / 2.0f) + exactFitRectPixels.X;
+            xnaViewportRect.Y = (int)Math.Floor((exactFitRectPixels.Height - xnaViewportRect.Height) / 2.0f) + exactFitRectPixels.Y;
+
+            viewportInPixels = new CCRect(xnaViewportRect);
+            xnaViewport = new Viewport(xnaViewportRect);
+
+            OnViewportChanged(this, null);
+        }
+    }
+}
+
---
+++ src/CCWindow.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CocosSharp
+{
+    public class CCWindow
+    {
+        public static string EVENT_PROJECTION_CHANGED = "window_projection_changed";
+        public static string EVENT_AFTER_UPDATE = "window_after_update";
+        public static string EVENT_AFTER_VISIT = "window_after_visit";
+        public static string EVENT_AFTER_DRAW = "window_after_draw";
+
+        CCEventCustom eventAfterDraw;
+        CCEventCustom eventAfterVisit;
+        CCEventCustom eventAfterUpdate;
+
+        List<CCDirector> sceneDirectors;
+
+
+        #region Properties
+
+        #if !NETFX_CORE
+        public CCAccelerometer Accelerometer { get; set; }
+        #endif
+
+        public bool GamePadEnabled { get; set; }
+        public virtual double AnimationInterval { get; set; }
+        public CCNode NotificationNode { get; set; }
+        public CCEventDispatcher EventDispatcher { get; private set; }
+
+        internal List<CCDirector> SceneDirectors { get { return sceneDirectors; } }
+        internal CCDrawManager DrawManager { get { return CCDrawManager.SharedDrawManager; } }
+        internal GameWindow XnaWindow { get; private set; }
+        internal GraphicsDeviceManager DeviceManager { get; private set; }
+
+        protected CCStats Stats { get; private set; }
+
+        public bool IsUseAlphaBlending
+        {
+            set 
+            {
+                if (value)
+                {
+                    DrawManager.BlendFunc(CCBlendFunc.AlphaBlend);
+                }
+                else
+                {
+                    DrawManager.BlendFunc(new CCBlendFunc(CCOGLES.GL_ONE, CCOGLES.GL_ZERO));
+                }
+            }
+        }
+
+        public bool IsUseDepthTesting
+        {
+            get { return DrawManager.DepthTest; }
+            set { DrawManager.DepthTest = value; }
+        }
+
+        public bool DisplayStats 
+        {
+            get { return Stats.IsEnabled; }
+            set { Stats.IsEnabled = value; }
+        }
+
+        public bool AllowUserResizing
+        {
+            get { return XnaWindow.AllowUserResizing; }
+            set { XnaWindow.AllowUserResizing = value; }
+        }
+
+        public bool FullScreen 
+        { 
+            get { return DeviceManager.IsFullScreen; }
+            set { DeviceManager.IsFullScreen = value; }
+        }
+
+        public bool PreferMultiSampling 
+        { 
+            get { return DeviceManager.PreferMultiSampling; }
+            set { DeviceManager.PreferMultiSampling = value; }
+        }
+
+        public CCSize WindowSizeInPixels
+        {
+            get 
+            { 
+                Rectangle winBounds = XnaWindow.ClientBounds;
+                return new CCSize(winBounds.Width, winBounds.Height); 
+            }
+            set 
+            {
+//                DrawManager.XnaGraphicsDeviceManager.PreferredBackBufferWidth = (int)value.Width;
+//                DrawManager.XnaGraphicsDeviceManager.PreferredBackBufferHeight = (int)value.Height;
+//                DrawManager.XnaGraphicsDeviceManager.ApplyChanges ();
+            }
+        }
+
+        public CCDisplayOrientation SupportedDisplayOrientations
+        {
+            get { return DrawManager.SupportedDisplayOrientations; }
+            set { DrawManager.SupportedDisplayOrientations = value; }
+        }
+
+        public CCDisplayOrientation CurrentDisplayOrientation
+        {
+            get { return (CCDisplayOrientation)XnaWindow.CurrentOrientation; }
+            set { DrawManager.CurrentDisplayOrientation = value; }
+        }
+
+        internal CCSize LandscapeWindowSizeInPixels
+        {
+            get { return CurrentDisplayOrientation.IsPortrait() ? WindowSizeInPixels.Inverted : WindowSizeInPixels; }
+        }
+
+        #endregion Properties
+
+
+        #region Constructors
+
+        internal CCWindow(CCSize screenSizeInPixels, GameWindow xnaWindow, GraphicsDeviceManager deviceManager)
+        {
+            sceneDirectors = new List<CCDirector>();
+
+            EventDispatcher = new CCEventDispatcher(this);
+            //Stats = new CCStats();
+
+            #if !NETFX_CORE
+            Accelerometer = new CCAccelerometer(this);
+            #endif
+
+            eventAfterDraw = new CCEventCustom(EVENT_AFTER_DRAW);
+            eventAfterDraw.UserData = this;
+            eventAfterVisit = new CCEventCustom(EVENT_AFTER_VISIT);
+            eventAfterVisit.UserData = this;
+            eventAfterUpdate = new CCEventCustom(EVENT_AFTER_UPDATE);
+            eventAfterUpdate.UserData = this;
+
+
+            IsUseAlphaBlending = true;
+            IsUseDepthTesting = false;
+
+            this.XnaWindow = xnaWindow;
+            xnaWindow.OrientationChanged += OnOrientationChanged;
+            xnaWindow.ClientSizeChanged += OnWindowSizeChanged;
+            xnaWindow.AllowUserResizing = true;
+
+            WindowSizeInPixels = screenSizeInPixels;
+
+
+
+            //Stats.Initialize();
+        }
+
+        #endregion Constructors
+
+
+        #region Scene director management
+
+        public void AddSceneDirector(CCDirector sceneDirector)
+        {
+            if (sceneDirector != null && !sceneDirectors.Contains(sceneDirector)) 
+            {
+                sceneDirectors.Add(sceneDirector);
+            }
+        }
+
+        public void RemoveSceneDirector(CCDirector sceneDirector)
+        {
+            sceneDirectors.Remove(sceneDirector);
+        }
+
+        public void RemoveAllSceneDirectors()
+        {
+            sceneDirectors.Clear();
+        }
+
+        #endregion Scene director management
+
+
+        #region Cleaning up
+
+        public void EndAllSceneDirectors()
+        {
+            foreach (CCDirector director in sceneDirectors) {
+                director.End();
+            }
+        }
+
+        internal void ReInitStats()
+        {
+            Stats = new CCStats();
+        }
+
+        #endregion Cleaning up
+
+
+        #region Orientation handling
+
+        void OnOrientationChanged(object sender, EventArgs e)
+        {
+            CurrentDisplayOrientation = (CCDisplayOrientation)XnaWindow.CurrentOrientation;
+
+            foreach(CCDirector director in sceneDirectors) 
+            {
+                if(director.RunningScene != null) 
+                {
+                    director.RunningScene.Viewport.DisplayOrientation = CurrentDisplayOrientation;
+                }
+            }
+        }
+
+        #endregion Orientation handling
+
+
+        #region Window resize handling
+
+        void OnWindowSizeChanged(object sender, EventArgs e)
+        {
+            CCSize landscapeWindowSize = LandscapeWindowSizeInPixels;
+
+            foreach(CCDirector director in sceneDirectors) 
+            {
+                if(director.RunningScene != null) 
+                {
+                    director.RunningScene.Viewport.LandscapeScreenSizeInPixels = landscapeWindowSize;
+                }
+            }
+        }
+
+        #endregion Window resize handling
+
+
+        #region Drawing and updating
+
+        public virtual void MainLoop(CCGameTime gameTime)
+        {
+            foreach (CCDirector director in sceneDirectors) 
+            {
+                if (director.IsPurgeDirectorInNextLoop) 
+                {
+                    director.PurgeDirector();
+                    director.IsPurgeDirectorInNextLoop = false;
+                } 
+                else
+                {
+                    Draw(gameTime);
+                }
+            }
+        }
+
+        protected void Draw(CCGameTime gameTime)
+        {
+            //Stats.UpdateStart();
+
+            DrawManager.PushMatrix();
+
+            foreach (CCDirector director in sceneDirectors) 
+            {
+                CCScene runningScene = director.RunningScene;
+
+                // draw the scene
+                if (runningScene != null) 
+                {
+                    runningScene.Visit();
+                    if (EventDispatcher.IsEventListenersFor(EVENT_AFTER_VISIT))
+                        EventDispatcher.DispatchEvent (eventAfterVisit);
+                }
+
+                // draw the notifications node
+                if (NotificationNode != null) 
+                {
+                    NotificationNode.Visit();
+                }
+
+                if (EventDispatcher.IsEventListenersFor (EVENT_AFTER_DRAW))
+                    EventDispatcher.DispatchEvent (eventAfterDraw);
+            }
+
+            DrawManager.PopMatrix();
+
+            //Stats.Draw(this);
+        }
+
+        internal void Update(float deltaTime)
+        {
+            //Stats.UpdateStart();
+
+            if(EventDispatcher.IsEventListenersFor(EVENT_AFTER_UPDATE)) 
+            {
+                EventDispatcher.DispatchEvent(eventAfterUpdate);
+            }
+
+            CCDisplayOrientation orientation = CurrentDisplayOrientation;
+            CCSize landscapeWindowSize = LandscapeWindowSizeInPixels;
+
+            foreach(CCDirector director in sceneDirectors) 
+            {
+                if(director.NextScene != null)
+                {
+                    director.NextScene.Viewport.DisplayOrientation = orientation;
+                    director.NextScene.Viewport.LandscapeScreenSizeInPixels = landscapeWindowSize;
+                    director.SetNextScene();
+                }
+            }
+
+            //Stats.UpdateEnd(deltaTime);
+        }
+
+        #endregion Drawing and updating
+
+    }
+}
