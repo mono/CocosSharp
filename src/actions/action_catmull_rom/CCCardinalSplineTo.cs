e63122d4e59912c3d10842156b5b3d32b5c3017b
Rami Tabbara <rtabbara@gmail.com>
2014-08-01 11:59:47 +1000

    Small cleanup of CCCardinalSplineTo:
    * reorganisation
    * indentation

--- src/actions/action_catmull_rom/CCCardinalSplineTo.cs
+++ src/actions/action_catmull_rom/CCCardinalSplineTo.cs
@@ -5,209 +5,212 @@ using System.Diagnostics;
 namespace CocosSharp
 {
     public class CCCardinalSplineTo : CCFiniteTimeAction
-	{
+    {
+        #region Properties
 
-		#region Constructors
+        public float Tension { get; protected set; }
+        public List<CCPoint> Points { get; protected set; }
 
-		public CCCardinalSplineTo (float duration, List<CCPoint> points, float tension) : base (duration)
-		{
-			Debug.Assert (points.Count > 0, "Invalid configuration. It must at least have one control point");
-
-			Points = points;
-			Tension = tension;
-		}
-
-		#endregion Constructors
+        #endregion Properties
 
 
-		public List<CCPoint> Points { get; protected set; }
+        #region Constructors
 
-		public float Tension { get; protected set; }
+        public CCCardinalSplineTo (float duration, List<CCPoint> points, float tension) : base (duration)
+        {
+            Debug.Assert (points.Count > 0, "Invalid configuration. It must at least have one control point");
 
-		protected internal override CCActionState StartAction(CCNode target)
-		{
-			return new CCCardinalSplineToState (this, target);
+            Points = points;
+            Tension = tension;
+        }
 
-		}
-
-		public override CCFiniteTimeAction Reverse ()
-		{
-			List<CCPoint> pReverse = new List<CCPoint> (Points);
-			pReverse.Reverse ();
-
-			return new CCCardinalSplineTo (Duration, pReverse, Tension);
-		}
-
-	}
-
-	internal class CCCardinalSplineToState : CCFiniteTimeActionState
-	{
-		protected float DeltaT { get; set; }
-
-		protected CCPoint PreviousPosition { get; set; }
-
-		protected CCPoint AccumulatedDiff { get; set; }
-
-		protected List<CCPoint> Points { get; set; }
-
-		protected float Tension { get; set; }
+        #endregion Constructors
 
 
-		public CCCardinalSplineToState (CCCardinalSplineTo action, CCNode target)
-			: base (action, target)
-		{ 
-			Points = action.Points;
-			Tension = action.Tension;
+        protected internal override CCActionState StartAction(CCNode target)
+        {
+            return new CCCardinalSplineToState (this, target);
 
-			DeltaT = 1f / Points.Count;
+        }
 
-			PreviousPosition = target.Position;
-			AccumulatedDiff = CCPoint.Zero;
-		}
+        public override CCFiniteTimeAction Reverse()
+        {
+            List<CCPoint> reverse = new List<CCPoint> (Points);
+            reverse.Reverse ();
 
+            return new CCCardinalSplineTo (Duration, reverse, Tension);
+        }
 
-		public override void Update (float time)
-		{
-			int p;
-			float lt;
+    }
 
-			// eg.
-			// p..p..p..p..p..p..p
-			// 1..2..3..4..5..6..7
-			// want p to be 1, 2, 3, 4, 5, 6
-			if (time == 1)
-			{
-				p = Points.Count - 1;
-				lt = 1;
-			}
-			else
-			{
-				p = (int)(time / DeltaT);
-				lt = (time - DeltaT * p) / DeltaT;
-			}
+    internal class CCCardinalSplineToState : CCFiniteTimeActionState
+    {
+        protected float DeltaT { get; set; }
 
-			// Interpolate    
-			var c = Points.Count - 1;
-			CCPoint pp0 = Points [Math.Min (c, Math.Max (p - 1, 0))];
-			CCPoint pp1 = Points [Math.Min (c, Math.Max (p + 0, 0))];
-			CCPoint pp2 = Points [Math.Min (c, Math.Max (p + 1, 0))];
-			CCPoint pp3 = Points [Math.Min (c, Math.Max (p + 2, 0))];
+        protected CCPoint PreviousPosition { get; set; }
 
-			CCPoint newPos = CCSplineMath.CCCardinalSplineAt (pp0, pp1, pp2, pp3, Tension, lt);
+        protected CCPoint AccumulatedDiff { get; set; }
 
-			// Support for stacked actions
-			CCNode node = Target;
-			CCPoint diff = node.Position - PreviousPosition;
-			if (diff.X != 0 || diff.Y != 0)
-			{
-				AccumulatedDiff = AccumulatedDiff + diff;
-				newPos = newPos + AccumulatedDiff;
-			}
+        protected List<CCPoint> Points { get; set; }
 
-			UpdatePosition (newPos);
-		}
-
-		public virtual void UpdatePosition (CCPoint newPos)
-		{
-			Target.Position = newPos;
-			PreviousPosition = newPos;
-		}
-
-	}
+        protected float Tension { get; set; }
 
 
+        public CCCardinalSplineToState (CCCardinalSplineTo action, CCNode target)
+            : base (action, target)
+        { 
+            Points = action.Points;
+            Tension = action.Tension;
 
-	public class CCCardinalSplineBy : CCCardinalSplineTo
-	{
+            DeltaT = 1f / Points.Count;
 
-		#region Constructors
-
-		public CCCardinalSplineBy (float duration, List<CCPoint> points, float tension) : base (duration, points, tension)
-		{
-		}
-
-		#endregion Constructors
-
-		protected internal override CCActionState StartAction(CCNode target)
-		{
-			return new CCCardinalSplineByState (this, target);
-
-		}
-
-		public override CCFiniteTimeAction Reverse ()
-		{
-			List<CCPoint> copyConfig = new List<CCPoint> (Points);
-
-			//
-			// convert "absolutes" to "diffs"
-			//
-			CCPoint p = copyConfig [0];
-
-			for (int i = 1; i < copyConfig.Count; ++i)
-			{
-				CCPoint current = copyConfig [i];
-				CCPoint diff = (current - p);
-				copyConfig [i] = diff;
-
-				p = current;
-			}
-
-			// convert to "diffs" to "reverse absolute"
-			copyConfig.Reverse ();
-
-			// 1st element (which should be 0,0) should be here too
-
-			p = copyConfig [copyConfig.Count - 1];
-			copyConfig.RemoveAt (copyConfig.Count - 1);
-
-			p = -p;
-			copyConfig.Insert (0, p);
-
-			for (int i = 1; i < copyConfig.Count; ++i)
-			{
-				CCPoint current = copyConfig [i];
-				current = -current;
-				CCPoint abs = current + p;
-				copyConfig [i] = abs;
-
-				p = abs;
-			}
-
-			return new CCCardinalSplineBy (Duration, copyConfig, Tension);
-		}
-
-	}
+            PreviousPosition = target.Position;
+            AccumulatedDiff = CCPoint.Zero;
+        }
 
 
-	internal class CCCardinalSplineByState : CCCardinalSplineToState
-	{
-		protected CCPoint StartPosition { get; set; }
+        public override void Update (float time)
+        {
+            int p;
+            float lt;
 
-		public CCCardinalSplineByState (CCCardinalSplineTo action, CCNode target)
-			: base (action, target)
-		{ 
-			StartPosition = target.Position;
-		}
+            // eg.
+            // p..p..p..p..p..p..p
+            // 1..2..3..4..5..6..7
+            // want p to be 1, 2, 3, 4, 5, 6
+            if (time == 1)
+            {
+                p = Points.Count - 1;
+                lt = 1;
+            }
+            else
+            {
+                p = (int)(time / DeltaT);
+                lt = (time - DeltaT * p) / DeltaT;
+            }
 
-		public override void UpdatePosition (CCPoint newPos)
-		{
-			Target.Position = newPos + StartPosition;
-			PreviousPosition = Target.Position;
-		}
+            // Interpolate    
+            var c = Points.Count - 1;
+            CCPoint pp0 = Points [Math.Min (c, Math.Max (p - 1, 0))];
+            CCPoint pp1 = Points [Math.Min (c, Math.Max (p + 0, 0))];
+            CCPoint pp2 = Points [Math.Min (c, Math.Max (p + 1, 0))];
+            CCPoint pp3 = Points [Math.Min (c, Math.Max (p + 2, 0))];
 
-	}
+            CCPoint newPos = CCSplineMath.CCCardinalSplineAt (pp0, pp1, pp2, pp3, Tension, lt);
 
-	public class CCCatmullRomTo : CCCardinalSplineTo
-	{
-		public CCCatmullRomTo (float dt, List<CCPoint> points) : base (dt, points, 0.5f)
-		{
-		}
-	}
+            // Support for stacked actions
+            CCNode node = Target;
+            CCPoint diff = node.Position - PreviousPosition;
+            if (diff.X != 0 || diff.Y != 0)
+            {
+                AccumulatedDiff = AccumulatedDiff + diff;
+                newPos = newPos + AccumulatedDiff;
+            }
 
-	public class CCCatmullRomBy : CCCardinalSplineBy
-	{
-		public CCCatmullRomBy (float dt, List<CCPoint> points) : base (dt, points, 0.5f)
-		{
-		}
-	}
+            UpdatePosition (newPos);
+        }
+
+        public virtual void UpdatePosition (CCPoint newPos)
+        {
+            Target.Position = newPos;
+            PreviousPosition = newPos;
+        }
+
+    }
+
+
+
+    public class CCCardinalSplineBy : CCCardinalSplineTo
+    {
+
+        #region Constructors
+
+        public CCCardinalSplineBy (float duration, List<CCPoint> points, float tension) : base (duration, points, tension)
+        {
+        }
+
+        #endregion Constructors
+
+        protected internal override CCActionState StartAction(CCNode target)
+        {
+            return new CCCardinalSplineByState (this, target);
+
+        }
+
+        public override CCFiniteTimeAction Reverse ()
+        {
+            List<CCPoint> copyConfig = new List<CCPoint> (Points);
+
+            //
+            // convert "absolutes" to "diffs"
+            //
+            CCPoint p = copyConfig [0];
+
+            for (int i = 1; i < copyConfig.Count; ++i)
+            {
+                CCPoint current = copyConfig [i];
+                CCPoint diff = (current - p);
+                copyConfig [i] = diff;
+
+                p = current;
+            }
+
+            // convert to "diffs" to "reverse absolute"
+            copyConfig.Reverse ();
+
+            // 1st element (which should be 0,0) should be here too
+
+            p = copyConfig [copyConfig.Count - 1];
+            copyConfig.RemoveAt (copyConfig.Count - 1);
+
+            p = -p;
+            copyConfig.Insert (0, p);
+
+            for (int i = 1; i < copyConfig.Count; ++i)
+            {
+                CCPoint current = copyConfig [i];
+                current = -current;
+                CCPoint abs = current + p;
+                copyConfig [i] = abs;
+
+                p = abs;
+            }
+
+            return new CCCardinalSplineBy (Duration, copyConfig, Tension);
+        }
+
+    }
+
+
+    internal class CCCardinalSplineByState : CCCardinalSplineToState
+    {
+        protected CCPoint StartPosition { get; set; }
+
+        public CCCardinalSplineByState (CCCardinalSplineTo action, CCNode target)
+            : base (action, target)
+        { 
+            StartPosition = target.Position;
+        }
+
+        public override void UpdatePosition (CCPoint newPos)
+        {
+            Target.Position = newPos + StartPosition;
+            PreviousPosition = Target.Position;
+        }
+
+    }
+
+    public class CCCatmullRomTo : CCCardinalSplineTo
+    {
+        public CCCatmullRomTo (float dt, List<CCPoint> points) : base (dt, points, 0.5f)
+        {
+        }
+    }
+
+    public class CCCatmullRomBy : CCCardinalSplineBy
+    {
+        public CCCatmullRomBy (float dt, List<CCPoint> points) : base (dt, points, 0.5f)
+        {
+        }
+    }
 }
