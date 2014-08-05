b3032b0515ffe5cfc69faf2a2234116620dbc83d
Kenneth J. Pouncey <kjpou@pt.lu>
2014-08-05 20:05:37 +0200

    Enhance CCDrawNode to add a DrawCircle and DrawSolidCircle

--- src/misc_nodes/CCDrawNode.cs
+++ src/misc_nodes/CCDrawNode.cs
@@ -7,7 +7,11 @@ namespace CocosSharp
 {
     public class CCDrawNode : CCNode
     {
-        CCRawList<VertexPositionColor> vertices;
+
+        const int DefaultBufferSize = 512;
+        CCRawList<VertexPositionColor> triangleVertices;
+        CCRawList<VertexPositionColor> lineVertices;
+
         bool dirty;
 
         /** draw a polygon with a fill color and line color */
@@ -26,7 +30,8 @@ namespace CocosSharp
         public CCDrawNode()
         {
             BlendFunc = CCBlendFunc.AlphaBlend;
-            vertices = new CCRawList<VertexPositionColor>(512);
+            triangleVertices = new CCRawList<VertexPositionColor>(DefaultBufferSize);
+            lineVertices = new CCRawList<VertexPositionColor>(DefaultBufferSize);
         }
 
         #endregion Constructors
@@ -60,7 +65,7 @@ namespace CocosSharp
 			
 				vert1.Position.X = x + pos.X;
 				vert1.Position.Y = y + pos.Y;
-				vertices.Add(vert1); // output vertex
+				triangleVertices.Add(vert1); // output vertex
 
 				//calculate the tangential vector 
 				//remember, the radial vector is (x, y) 
@@ -78,13 +83,163 @@ namespace CocosSharp
 
 				vert1.Position.X = x + pos.X;
 				vert1.Position.Y = y + pos.Y;
-				vertices.Add(vert1); // output vertex
+				triangleVertices.Add(vert1); // output vertex
 
-				vertices.Add(verticeCenter);
+				triangleVertices.Add(verticeCenter);
 			} 
 
 			dirty = true;
 		}
+
+        // See http://slabode.exofire.net/circle_draw.shtml
+        // An Efficient Way to Draw Approximate Circles in OpenGL
+        // Try to keep from calculating Cos and Sin of values everytime and just use
+        // add and subtract where possible to calculate the values.
+        public void DrawCircle(CCPoint pos, float radius, CCColor4B color)
+        {
+            var cl = color.ToColor();
+
+            var segments = 10 * (float)Math.Sqrt(radius);  //<- Let's try to guess at # segments for a reasonable smoothness
+
+            float theta = MathHelper.Pi * 2.0f / segments;
+            float tangetial_factor = (float)Math.Tan(theta);   //calculate the tangential factor 
+
+            float radial_factor = (float)Math.Cos(theta);   //calculate the radial factor 
+
+            float x = radius;  //we start at angle = 0 
+            float y = 0;
+
+            var verticeCenter = new VertexPositionColor(new Vector3(pos.X, pos.Y, 0), cl);
+            var vert1 = new VertexPositionColor(Vector3.Zero, cl);
+            float tx = 0;
+            float ty = 0;
+
+            for (int i = 0; i < segments; i++)
+            {
+
+                vert1.Position.X = x + pos.X;
+                vert1.Position.Y = y + pos.Y;
+                lineVertices.Add(vert1); // output vertex
+
+                //calculate the tangential vector 
+                //remember, the radial vector is (x, y) 
+                //to get the tangential vector we flip those coordinates and negate one of them 
+                tx = -y;
+                ty = x;
+
+                //add the tangential vector 
+                x += tx * tangetial_factor;
+                y += ty * tangetial_factor;
+
+                //correct using the radial factor 
+                x *= radial_factor;
+                y *= radial_factor;
+
+                vert1.Position.X = x + pos.X;
+                vert1.Position.Y = y + pos.Y;
+                lineVertices.Add(vert1); // output vertex
+
+            }
+
+            dirty = true;
+        }
+
+        // See http://slabode.exofire.net/circle_draw.shtml
+        // An Efficient Way to Draw Approximate Circles in OpenGL
+        // Try to keep from calculating Cos and Sin of values everytime and just use
+        // add and subtract where possible to calculate the values.
+        public void DrawSolidCircle(CCPoint pos, float radius, CCColor4B color)
+        {
+            var cl = color.ToColor();
+            //radius = 53;
+            var segments = 10 * (float)Math.Sqrt(radius);  //<- Let's try to guess at # segments for a reasonable smoothness
+
+            float theta = MathHelper.Pi * 2.0f / segments;
+            float tangetial_factor = (float)Math.Tan(theta);   //calculate the tangential factor 
+
+            float radial_factor = (float)Math.Cos(theta);   //calculate the radial factor 
+
+            float x = radius;  //we start at angle = 0 
+            float y = 0;
+
+            var verticeCenter = new VertexPositionColor(new Vector3(pos.X, pos.Y, 0), cl);
+            var vert1 = new VertexPositionColor(Vector3.Zero, cl);
+            float tx = 0;
+            float ty = 0;
+
+            var first = new VertexPositionColor(Vector3.Zero, cl);
+            first.Position.X = x + pos.X;
+            first.Position.Y = y + pos.Y;
+
+            for (int i = 1; i < segments; i++)
+            {
+                triangleVertices.Add(verticeCenter);
+
+                vert1.Position.X = x + pos.X;
+                vert1.Position.Y = y + pos.Y;
+                triangleVertices.Add(vert1); // output vertex
+
+                //calculate the tangential vector 
+                //remember, the radial vector is (x, y) 
+                //to get the tangential vector we flip those coordinates and negate one of them 
+                tx = -y;
+                ty = x;
+
+                //add the tangential vector 
+                x += tx * tangetial_factor;
+                y += ty * tangetial_factor;
+
+                //correct using the radial factor 
+                x *= radial_factor;
+                y *= radial_factor;
+
+                vert1.Position.X = x + pos.X;
+                vert1.Position.Y = y + pos.Y;
+                triangleVertices.Add(vert1); // output vertex
+            }
+
+            // adjust our offset so that we do not have any drawing overlapp.
+            triangleVertices.Add(first);
+            vert1.Position.X -= 0.1f;
+            triangleVertices.Add(vert1);
+            triangleVertices.Add(verticeCenter);
+
+            dirty = true;
+        }
+
+        //public override void DrawCircle(CCPoint center, float radius, CCColor4B color)
+        //{
+        //    const float increment = Math.PI * 2.0f / CircleSegments;
+        //    float theta = 0.0f;
+
+        //    var col = color;
+        //    var colorOutline = col.ToCCColor4B();
+
+        //    CCVector2 centr = center.ToCCVector2();
+        //    CCVector2 thetaV = CCVector2.Zero;
+
+
+        //    for (int i = 0, count = CircleSegments; i < count; i++)
+        //    {
+
+        //        thetaV.X = cp.cpfcos(theta);
+        //        thetaV.Y = cp.cpfsin(theta);
+        //        CCVector2 v1 = centr + Convert.ToSingle(radius) * thetaV;
+
+        //        thetaV.X = cp.cpfcos(theta + increment);
+        //        thetaV.Y = cp.cpfsin(theta + increment);
+        //        CCVector2 v2 = centr +
+        //                     Convert.ToSingle(radius) *
+        //                     thetaV;
+
+        //        primitiveBatch.AddVertex(ref v1, colorOutline, PrimitiveType.LineList);
+        //        primitiveBatch.AddVertex(ref v2, colorOutline, PrimitiveType.LineList);
+
+        //        theta += increment;
+        //    }
+        //}
+
+
 
         public void DrawSegment(CCPoint from, CCPoint to, float radius, CCColor4F color)
         {
@@ -108,22 +263,22 @@ namespace CocosSharp
 			var v3 = a + nw;
 
 			// Triangles from beginning to end
-			vertices.Add(new VertexPositionColor(v1.ToVector3(), cl));
-			vertices.Add(new VertexPositionColor(v2.ToVector3(), cl));
-			vertices.Add(new VertexPositionColor(v0.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v1.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v2.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v0.ToVector3(), cl));
 
-			vertices.Add(new VertexPositionColor(v1.ToVector3(), cl));
-			vertices.Add(new VertexPositionColor(v2.ToVector3(), cl));
-			vertices.Add(new VertexPositionColor(v3.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v1.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v2.ToVector3(), cl));
+			triangleVertices.Add(new VertexPositionColor(v3.ToVector3(), cl));
 
 
             dirty = true;
         }
 
-        public void DrawCircle(CCPoint center, float radius, CCColor4B color)
-        {
-            DrawCircle(center, radius, CCMacros.CCDegreesToRadians(360f), 360, color);
-        }
+        //public void DrawCircle(CCPoint center, float radius, CCColor4B color)
+        //{
+        //    DrawCircle(center, radius, CCMacros.CCDegreesToRadians(360f), 360, color);
+        //}
 
         public void DrawCircle(CCPoint center, float radius, float angle, int segments, CCColor4B color)
         {
@@ -145,6 +300,7 @@ namespace CocosSharp
             CCColor4F cf = new CCColor4F(color.R/255f, color.G/255f, color.B/255f, color.A/255f);
             DrawPolygon(verts.ToArray(), verts.Count, cf, 0, new CCColor4F(0f, 0f, 0f, 0f));
         }
+
 
         public void DrawRect(CCRect rect, CCColor4B color)
         {
@@ -187,9 +343,9 @@ namespace CocosSharp
                 var v1 = verts[i + 1] - (extrude[i + 1].offset * inset);
                 var v2 = verts[i + 2] - (extrude[i + 2].offset * inset);
 
-                vertices.Add(new VertexPositionColor(v0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
-                vertices.Add(new VertexPositionColor(v1.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
-                vertices.Add(new VertexPositionColor(v2.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                triangleVertices.Add(new VertexPositionColor(v0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                triangleVertices.Add(new VertexPositionColor(v1.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                triangleVertices.Add(new VertexPositionColor(v2.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
             }
 
             for (int i = 0; i < count; i++)
@@ -210,13 +366,13 @@ namespace CocosSharp
                     var outer0 = (v0 + (offset0 * borderWidth));
                     var outer1 = (v1 + (offset1 * borderWidth));
 
-                    vertices.Add(new VertexPositionColor(inner0.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
-                    vertices.Add(new VertexPositionColor(inner1.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
-                    vertices.Add(new VertexPositionColor(outer1.ToVector3(), borderColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(inner0.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
+                    triangleVertices.Add(new VertexPositionColor(inner1.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
+                    triangleVertices.Add(new VertexPositionColor(outer1.ToVector3(), borderColor.ToColor())); //__t(n0)
 
-                    vertices.Add(new VertexPositionColor(inner0.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
-                    vertices.Add(new VertexPositionColor(outer0.ToVector3(), borderColor.ToColor())); //__t(n0)
-                    vertices.Add(new VertexPositionColor(outer1.ToVector3(), borderColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(inner0.ToVector3(), borderColor.ToColor())); //__t(v2fneg(n0))
+                    triangleVertices.Add(new VertexPositionColor(outer0.ToVector3(), borderColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(outer1.ToVector3(), borderColor.ToColor())); //__t(n0)
                 }
                 else
                 {
@@ -225,13 +381,13 @@ namespace CocosSharp
                     var outer0 = (v0 + (offset0 * 0.5f));
                     var outer1 = (v1 + (offset1 * 0.5f));
 
-                    vertices.Add(new VertexPositionColor(inner0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
-                    vertices.Add(new VertexPositionColor(inner1.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
-                    vertices.Add(new VertexPositionColor(outer1.ToVector3(), fillColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(inner0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                    triangleVertices.Add(new VertexPositionColor(inner1.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                    triangleVertices.Add(new VertexPositionColor(outer1.ToVector3(), fillColor.ToColor())); //__t(n0)
 
-                    vertices.Add(new VertexPositionColor(inner0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
-                    vertices.Add(new VertexPositionColor(outer0.ToVector3(), fillColor.ToColor())); //__t(n0)
-                    vertices.Add(new VertexPositionColor(outer1.ToVector3(), fillColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(inner0.ToVector3(), fillColor.ToColor())); //__t(v2fzero)
+                    triangleVertices.Add(new VertexPositionColor(outer0.ToVector3(), fillColor.ToColor())); //__t(n0)
+                    triangleVertices.Add(new VertexPositionColor(outer1.ToVector3(), fillColor.ToColor())); //__t(n0)
                 }
             }
 
@@ -240,7 +396,7 @@ namespace CocosSharp
 
         public void Clear()
         {
-            vertices.Clear();
+            triangleVertices.Clear();
         }
 
         protected override void Draw()
@@ -253,7 +409,35 @@ namespace CocosSharp
 
             Window.DrawManager.TextureEnabled = false;
             Window.DrawManager.BlendFunc(BlendFunc);
-            Window.DrawManager.DrawPrimitives(PrimitiveType.TriangleList, vertices.Elements, 0, vertices.Count / 3);
+            FlushTriangles();
+            FlushLines();
         }
+
+        void FlushTriangles()
+        {
+            var triangleVertsCount = triangleVertices.Count;
+            if (triangleVertsCount >= 3)
+            {
+                int primitiveCount = triangleVertsCount / 3;
+                // submit the draw call to the graphics card
+                Window.DrawManager.DrawPrimitives(PrimitiveType.TriangleList, triangleVertices.Elements, 0, primitiveCount);
+                triangleVertices.Clear();
+                //DrawManager.DrawCount++;
+            }
+        }
+
+        void FlushLines()
+        {
+            var lineVertsCount = lineVertices.Count;
+            if (lineVertsCount >= 2)
+            {
+                int primitiveCount = lineVertsCount / 2;
+                // submit the draw call to the graphics card
+                 Window.DrawManager.DrawPrimitives(PrimitiveType.LineList, lineVertices.Elements, 0, primitiveCount);
+                 lineVertices.Clear();
+                //DrawManager.DrawCount++;
+            }
+        }
+
     }
 }
