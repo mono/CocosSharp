fef02add130ec4f309cf58b09aba88a107f5f0ad
kjpou1 <kjpou@pt.lu>
2014-08-09 07:01:55 +0200

    Add properties StartLocation and PreviousLocation.  Delta now uses world coordinates to calculate the difference

--- src/predefine/CCTouch.cs
+++ src/predefine/CCTouch.cs
@@ -15,35 +15,51 @@ namespace CocosSharp
         CCPoint startPoint;
 
         internal CCNode Target { get; set; }
+
         #region Properties
 
         public int Id { get; private set; }
 
+        // returns the current touch location in world coordinates 
         public CCPoint Location
         {
             get { return Target.ScreenToWorldspace(point); }
         }
 
-
+        // returns the current touch location in screen coordinates 
         public CCPoint LocationOnScreen
         {
             get { return point; }
         }
 
-        /** returns the start touch location in screen coordinates */
+        // returns the start touch location in world coordinates 
+        public CCPoint StartLocation
+        {
+            get { return Target.ScreenToWorldspace(startPoint); }
+        }
+
+        // returns the start touch location in screen coordinates 
         public CCPoint StartLocationOnScreen
         {
             get { return startPoint; }
         }
 
+        // returns the previous touch location in world coordinates 
+        public CCPoint PreviousLocation
+        {
+            get { return Target.ScreenToWorldspace(prevPoint); }
+        }
+
+        // returns the previous touch location in screen coordinates 
         public CCPoint PreviousLocationOnScreen
         {
             get { return prevPoint; }
         }
 
+        // returns the delta position between the current location and the previous location in world coordinates
         public CCPoint Delta
         {
-            get { return LocationOnScreen - PreviousLocationOnScreen; }
+            get { return Location - PreviousLocation; }
         }
 
         #endregion Properties
