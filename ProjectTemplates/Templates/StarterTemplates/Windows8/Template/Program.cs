6dc5c89d3f882d7b9e4d508e726c5c52617b6d1e
Kenneth Pouncey <kjpou@pt.lu>
2014-08-25 15:41:21 +0200

    Fixes to Windows8/WindowsStore/WinRT templates.  Fix to work in VS2012 and VS2013.

--- ProjectTemplates/Templates/StarterTemplates/Windows8/Template/Program.cs
+++ ProjectTemplates/Templates/StarterTemplates/Windows8/Template/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System;
+using CocosSharp;
 
 namespace $safeprojectname$
 {
     /// <summary>
     /// The main class.
     /// </summary>
-    public static class Program
+     public static class Program 
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
+        static void Main() 
         {
-            var factory = new MonoGame.Framework.GameFrameworkViewSource<Game1>();
-            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+            CCApplication.Create(new AppDelegate());
         }
     }
 }
