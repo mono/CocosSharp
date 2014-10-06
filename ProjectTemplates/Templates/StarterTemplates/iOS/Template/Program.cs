1fec9963eb758c0b73ffcb9193c7efd18d852eab
Kenneth Pouncey <kjpou@pt.lu>
2014-10-06 23:09:11 +0200

    Update to build iOS templates for Visual Studio

--- ProjectTemplates/Templates/StarterTemplates/iOS/Template/Program.cs
+++ ProjectTemplates/Templates/StarterTemplates/iOS/Template/Program.cs
@@ -4,28 +4,32 @@ using System.Diagnostics;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
+using CocosSharp;
+
 namespace $safeprojectname$
 {
-	[Register ("AppDelegate")]
-	class Program : UIApplicationDelegate 
-	{
-		private Game1 game;
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        public override void FinishedLaunching(UIApplication app)
+        {
+            CCApplication application = new CCApplication();
+            application.ApplicationDelegate = new AppDelegate();
 
-		public override void FinishedLaunching (UIApplication app)
-		{
-			// Fun begins..
-			game = new Game1();
-			game.Run();
-		}
-		
-		// This is the main entry point of the application.
-		static void Main (string[] args)
-		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegate");
-		}
-	}
+            application.StartGame();
+        }
+
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
 
 }
+
+
 
