020d61ac2c1f99e33a5485eeaa6b892dcd439c02
Rami Tabbara <rtabbara@gmail.com>
2014-10-24 21:51:57 +1100

    Removing redundant scripts

--- scripts/build-Android.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat x86"
-set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;c:\windows\system32;c:\nant-0.92\bin
-nant -buildfile:android.build > android.out 2>&1
-nant -buildfile:deploy-Android.build > deploy.out 2>&1
