2c312b8d75a545739dddcc7a9771df3bbd2f7e74
Rami Tabbara <rtabbara@gmail.com>
2014-11-21 18:44:18 +1100

    Close #60 XS Project Templates: Showcase new TexelToContentSizeRatio feature

--- ProjectTemplates/XamarinStudio/Common/GameAppDelegate.cs
+++ ProjectTemplates/XamarinStudio/Common/GameAppDelegate.cs
@@ -8,17 +8,32 @@ namespace ${SolutionName}
     {
         public override void ApplicationDidFinishLaunching(CCApplication application, CCWindow mainWindow)
         {
-            //application.AllowUserResizing = true;
             application.PreferMultiSampling = false;
             application.ContentRootDirectory = "Content";
             application.ContentSearchPaths.Add("animations");
             application.ContentSearchPaths.Add("fonts");
-            application.ContentSearchPaths.Add("images");
             application.ContentSearchPaths.Add("sounds");
 
             CCSize windowSize = mainWindow.WindowSizeInPixels;
-            mainWindow.SetDesignResolutionSize(windowSize.Width, windowSize.Height, CCSceneResolutionPolicy.ShowAll);
 
+            float desiredWidth = 1024.0f;
+            float desiredHeight = 768.0f;
+            
+            // This will set the world bounds to be (0,0, w, h)
+            // CCSceneResolutionPolicy.ShowAll will ensure that the aspect ratio is preserved
+            mainWindow.SetDesignResolutionSize(desiredWidth, desiredHeight, CCSceneResolutionPolicy.ShowAll);
+            
+            // Determine whether to use the high or low def versions of our images
+            // Make sure the default texel to content size ratio is set correctly
+            // Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+            if(desiredWidth < windowSize.Width) {
+                application.ContentSearchPaths.Add("images/hd");
+                CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+            } else {
+                application.ContentSearchPaths.Add("images/ld");
+                CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+            }
+            
             CCScene scene = new CCScene(mainWindow);
             GameLayer gameLayer = new GameLayer();
 
