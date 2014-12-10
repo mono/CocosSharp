3d9e9322a4ece29fb14240dd3e6ac2eab8cc7bc2
kjpou1 <kjpou@pt.lu>
2014-12-10 16:38:37 +0100

    Update UpdReleaseVer.sh script to include updating the NuGet package version references automatically for Windows Phone 8 templates. + Add update of source.extension.vsixmanifest version for each of the templates that will be built.

--- tools/scripts/UpdReleaseVer.sh
+++ tools/scripts/UpdReleaseVer.sh
@@ -13,7 +13,14 @@ find ../../ProjectTemplates/NuGet/*.nuspec -type f -exec sed -i '' "s#ReleaseNot
 # Update Android Template NuGet version to be included in built Template
 sed -i '' "s/version=\".*\" \/>/version=\"$1\" \/>/g" ../../ProjectTemplates/Templates/StarterTemplates/Android/Template/EmptyProject.Android.vstemplate 
 sed -i '' "s/CocosSharp.Android\..*.nupkg/CocosSharp.Android.$1.nupkg/g" ../../ProjectTemplates/Templates/StarterTemplates/Android/VSIX/EmptyProject.Android/EmptyProject.Android/EmptyProject.Android.csproj
+sed -i '' "s#<Version>.*</Version>#<Version>$1</Version>#g" ../../ProjectTemplates/Templates/StarterTemplates/Android/VSIX/EmptyProject.Android/EmptyProject.Android/source.extension.vsixmanifest
 
 # Update iOS Template NuGet version to be included in built Template
 sed -i '' "s/version=\".*\" \/>/version=\"$1\" \/>/g" ../../ProjectTemplates/Templates/StarterTemplates/iOS/Template/EmptyProject.iOS.vstemplate 
 sed -i '' "s/CocosSharp.iOS\..*.nupkg/CocosSharp.iOS.$1.nupkg/g" ../../ProjectTemplates/Templates/StarterTemplates/iOS/VSIX/EmptyProject.iOS/EmptyProject.iOS/EmptyProject.iOS.csproj
+sed -i '' "s#<Version>.*</Version>#<Version>$1</Version>#g" ../../ProjectTemplates/Templates/StarterTemplates/iOS/VSIX/EmptyProject.iOS/EmptyProject.iOS/source.extension.vsixmanifest
+
+# Update Windows Phone 8 Template NuGet version to be included in built Template
+sed -i '' "s/version=\".*\" \/>/version=\"$1\" \/>/g" ../../ProjectTemplates/Templates/StarterTemplates/WP8/Template/EmptyProject.WP8.vstemplate 
+sed -i '' "s/CocosSharp.WindowsPhone8\..*.nupkg/CocosSharp.WindowsPhone8.$1.nupkg/g" ../../ProjectTemplates/Templates/StarterTemplates/WP8/VSIX/EmptyProject.WP8/EmptyProject.WP8/EmptyProject.WP8.csproj
+sed -i '' "s#<Version>.*</Version>#<Version>$1</Version>#g" ../../ProjectTemplates/Templates/StarterTemplates/WP8/VSIX/EmptyProject.WP8/EmptyProject.WP8/source.extension.vsixmanifest
