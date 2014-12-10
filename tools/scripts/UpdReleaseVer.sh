3110467a13b614db3efc6076d8944deb7b0a172f
kjpou1 <kjpou@pt.lu>
2014-12-10 11:44:43 +0100

    Update UpdReleaseVer.sh script to include updating the NuGet package version references automatically for iOS templates

--- tools/scripts/UpdReleaseVer.sh
+++ tools/scripts/UpdReleaseVer.sh
@@ -13,3 +13,7 @@ find ../../ProjectTemplates/NuGet/*.nuspec -type f -exec sed -i '' "s#ReleaseNot
 # Update Android Template NuGet version to be included in built Template
 sed -i '' "s/version=\".*\" \/>/version=\"$1\" \/>/g" ../../ProjectTemplates/Templates/StarterTemplates/Android/Template/EmptyProject.Android.vstemplate 
 sed -i '' "s/CocosSharp.Android\..*.nupkg/CocosSharp.Android.$1.nupkg/g" ../../ProjectTemplates/Templates/StarterTemplates/Android/VSIX/EmptyProject.Android/EmptyProject.Android/EmptyProject.Android.csproj
+
+# Update iOS Template NuGet version to be included in built Template
+sed -i '' "s/version=\".*\" \/>/version=\"$1\" \/>/g" ../../ProjectTemplates/Templates/StarterTemplates/iOS/Template/EmptyProject.iOS.vstemplate 
+sed -i '' "s/CocosSharp.iOS\..*.nupkg/CocosSharp.iOS.$1.nupkg/g" ../../ProjectTemplates/Templates/StarterTemplates/iOS/VSIX/EmptyProject.iOS/EmptyProject.iOS/EmptyProject.iOS.csproj
