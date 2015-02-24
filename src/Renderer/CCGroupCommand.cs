4503e78afed564a4c9d17937fd35650f757177f2
kjpou1 <kjpou@pt.lu>
2015-02-24 05:56:09 +0100

    Add CCGroupCommand which allows for rendering of RenderCommands in a group.

---
+++ src/Renderer/CCGroupCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CocosSharp
+{
+    public class CCGroupCommand : CCRenderCommand
+    {
+        internal bool Begin { get; set; }
+
+        public CCGroupCommand(float globalZOrder, bool begin = true )
+            : base (globalZOrder)
+        {
+            CommandType = CommandType.GROUP_COMMAND;
+            Begin = begin;
+        }
+
+        internal override void Execute(CCDrawManager drawManager)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
+
