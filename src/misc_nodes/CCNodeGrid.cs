a0f5309c31bb8a5229ad3a809038bbd8dd216f17
kjpou1 <kjpou@pt.lu>
2015-02-24 16:07:57 +0100

    Add CCNodeGrid which allows the hosting of a target node that will have a CCGridAction effect applied to it.
    * CCNode allows CCGridAction actions against them and still retains this capability for now.
    * In the near future the CCNode functionality will become obsolete and will be replaced by CCNodeGrid.
    * Moving to the usage of CCNodeGrid functionality will simplify CCNode's drawing code which is overly complicated with Grid code.
    * Developers should start migrating their code now.

---
+++ src/misc_nodes/CCNodeGrid.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace CocosSharp
+{
+    public class CCNodeGrid : CCNode
+    {
+
+        private CCGridBase grid;
+
+        /// <summary>
+        /// Gets or sets the target that the effect is applied to.
+        /// </summary>
+        /// <value>The target.</value>
+        public CCNode Target { get; set; }
+
+        public CCNodeGrid () : base ()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the grid object that is used when applying effects.
+        /// </summary>
+        /// <value>The grid.</value>
+        public new CCGridBase Grid 
+        { 
+            get { return grid; }
+            set 
+            {
+                grid = value;
+                if(value != null) 
+                {
+                    grid.Scene = Scene;
+                    grid.Layer = Layer;
+                }
+            }
+        }
+
+        public override void Visit ()
+        {
+            bool renderTarget = CCDrawManager.SharedDrawManager.CurrentRenderTarget != null;
+
+            if ((!Visible || Scene == null) && !renderTarget)
+            {
+                return;
+            }
+
+            var drawManager = DrawManager;
+
+            // Set camera view/proj matrix even if ChildClippingMode is None
+            if(Camera != null && !renderTarget)
+            {
+                drawManager.ViewMatrix = Camera.ViewMatrix;
+                drawManager.ProjectionMatrix = Camera.ProjectionMatrix;
+            }
+
+
+            drawManager.PushMatrix();
+
+            if (Grid != null && Grid.Active)
+            {
+                OnGridBeginDraw ();
+            }
+
+            Transform (drawManager);
+
+            if (Target != null)
+                Target.Visit ();
+
+            int i = 0;
+
+            if ((Children != null) && (Children.Count > 0))
+            {
+                SortAllChildren();
+
+                CCNode[] elements = Children.Elements;
+                int count = Children.Count;
+
+                // draw children zOrder < 0
+                for (; i < count; ++i)
+                {
+                    if (elements[i].LocalZOrder < 0)
+                    {
+                        // don't break loop on invisible children
+                        if (elements[i].Visible)
+                        {
+                            elements[i].Visit();
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                // self draw
+                Draw();
+                // draw the children
+                for (; i < count; ++i)
+                {
+                    // Draw the z >= 0 order children next.
+                    if (elements[i].Visible)
+                    {
+                        elements[i].Visit();
+                    }
+                }
+            }
+            else
+            {
+                // self draw
+                Draw();
+            }
+
+            if (Grid != null && Grid.Active)
+            {
+                OnGridEndDraw();
+                //drawManager.SetIdentityMatrix();
+                //Grid.Blit();
+            }
+
+            drawManager.PopMatrix();
+
+        }
+
+        protected virtual void OnGridBeginDraw ()
+        {
+            if (Grid != null && Grid.Active)
+            {
+                Grid.BeforeDraw();
+            }
+        }
+
+        protected virtual void OnGridEndDraw ()
+        {
+            if (Grid != null && Grid.Active)
+            {
+                Grid.AfterDraw(this);
+                DrawManager.SetIdentityMatrix();
+                Grid.Blit();
+            }
+
+        }
+
+
+    }
+}
+
