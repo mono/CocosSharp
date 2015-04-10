ab00243ebdf19cf9dd67da3ea41c3cd145145787
Kenneth Pouncey <kjpou@pt.lu>
2015-04-10 10:51:44 +0200

    Update CocosSharp release version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyInformationalVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
