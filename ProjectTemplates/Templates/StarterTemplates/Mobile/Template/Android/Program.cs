6c8e821a9fd29baa6da78f1462a281138baab195
kjpou1 <kjpou@pt.lu>
2015-06-05 15:16:29 +0200

    Windows Cross-platform PCL project template for mobile devices.
    See issue: https://github.com/mono/CocosSharp/issues/232

---
+++ ProjectTemplates/Templates/StarterTemplates/Mobile/Template/Android/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Diagnostics;
+
+using Android.Content.PM;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Views;
+using Android.Widget;
+using Uri = Android.Net.Uri;
+using Microsoft.Xna.Framework;
+
+using CocosSharp;
+using $ext_projectname$.Shared;
+
+namespace $safeprojectname$
+{
+
+    [Activity(Label = "$safeprojectname$"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+    public class Program : AndroidGameActivity
+    {
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            CCApplication application = new CCApplication();
+			application.ApplicationDelegate = new AppDelegate();
+
+			this.SetContentView(application.AndroidContentView);
+
+			application.StartGame();
+
+        }        
+    }
+
+
+}
+
