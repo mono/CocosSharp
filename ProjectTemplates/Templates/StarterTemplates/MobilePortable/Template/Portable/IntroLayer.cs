ffafe33f634cf3feae9c0b047d0916848fb8e66c
kjpou1 <kjpou@pt.lu>
2015-06-09 10:29:23 +0200

    Windows Cross-platform PCL project template for mobile devices.
    See issue: https://github.com/mono/CocosSharp/issues/232

---
+++ ProjectTemplates/Templates/StarterTemplates/MobilePortable/Template/Portable/IntroLayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using CocosSharp;
+using Microsoft.Xna.Framework;
+
+namespace $safeprojectname$
+{
+    public class IntroLayer : CCLayerColor
+    {
+
+        // Define a label variable
+        CCLabel label;
+
+        public IntroLayer()
+            : base(CCColor4B.Blue)
+        {
+
+            // create and initialize a Label
+            label = new CCLabel("Hello CocosSharp", "fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
+
+            // add the label as a child to this Layer
+            AddChild(label);
+
+        }
+
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+
+            // Use the bounds to layout the positioning of our drawable assets
+            var bounds = VisibleBoundsWorldspace;
+
+            // position the label on the center of the screen
+            label.Position = bounds.Center;
+
+            // Register for touch events
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesEnded = OnTouchesEnded;
+            AddEventListener(touchListener, this);
+        }
+
+        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            if (touches.Count > 0)
+            {
+                // Perform touch handling here
+            }
+        }
+    }
+}
+
