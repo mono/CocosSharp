ca3a4f6b7e360debdce5e73b4f972ccfb32809a8
Kenneth Pouncey <kjpou@pt.lu>
2015-06-09 09:53:13 +0200

    Add Box2D to PCL project so it can be referenced in PCL projects.
    See issue https://github.com/mono/CocosSharp/issues/233

---
+++ PCL/Box2DPCLShared/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle ("CocosSharp.PCL.Shared")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCompany("Open Source Software Provided As-Is")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0.0")]
