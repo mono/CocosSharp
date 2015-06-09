256243d74b2574a33ac9e1b50d7f1d125b37d215
Rami Tabbara <rtabbara@gmail.com>
2015-06-09 23:25:50 +1000

    Initial commit / Skeleton for embeddable GameView for Android

---
+++ src/EmbeddableView/CCGameView.Android.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Content;
+using Android.Util;
+
+using OpenTK;
+using OpenTK.Platform.Android;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+
+
+namespace CocosSharp
+{
+    public partial class CCGameView : AndroidGameView
+    {
+        public CCGameView(Context context) 
+            : base(context)
+        {
+            Initialise();
+        }
+
+        public CCGameView(Context context, IAttributeSet attrs) 
+            : base(context, attrs)
+        {
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            RenderOnUIThread = false;
+            FocusableInTouchMode = true;
+
+            RequestFocus();
+        }
+
+        void PlatformStartGame()
+        {
+            Run();
+        }
+
+        protected override void CreateFrameBuffer()
+        {
+            ContextRenderingApi = GLVersion.ES2;
+
+            // Fetch desired depth / stencil size
+            // Need to more robustly handle
+
+            try {
+                base.CreateFrameBuffer();
+                PlatformStartGame();
+                return;
+            } catch (Exception ex) {
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            if (GraphicsContext == null || GraphicsContext.IsDisposed)
+                return;
+
+            if (!GraphicsContext.IsCurrent)
+                MakeCurrent();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (!GraphicsContext.IsCurrent)
+                MakeCurrent();
+
+            // GraphicsDevice.Clear(Color.Black);
+            // Testing frame rendering
+            GL.ClearColor(Color4.AliceBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            PlatformPresent();
+        }
+
+        void PlatformPresent()
+        {
+            try
+            {
+                if (GraphicsDevice != null)
+                    GraphicsDevice.Present();
+
+                SwapBuffers();
+            }
+            catch (Exception ex)
+            {
+                Android.Util.Log.Error("Error in swap buffers", ex.ToString());
+            }
+        }
+    }
+}
+
---
+++ src/EmbeddableView/CCGameView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CocosSharp
+{
+    public partial class CCGameView
+    {
+        bool gameStarted;
+
+        internal GraphicsDevice GraphicsDevice { get; set; }
+
+        public void StartGame()
+        {
+            if(!gameStarted)
+            {
+                PlatformStartGame();
+                gameStarted = true;
+            }
+        }
+
+        internal void Present()
+        {
+            PlatformPresent();
+        }
+    }
+}
+
