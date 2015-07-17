94e7fcb06263751d60de125f7ad538f17fba1360
Rami Tabbara <rtabbara@gmail.com>
2015-07-17 22:02:54 +1000

    Forms : CCGameView.iOS : Ensure frame buffer is created when view constructed / attach depth buffer
    *  By default iPhoneOSGameView does not incorporate a depth buffer so we need to add one

--- src/EmbeddableView/CCGameView.iOS.cs
+++ src/EmbeddableView/CCGameView.iOS.cs
@@ -2,11 +2,15 @@
 using UIKit;
 using Foundation;
 using ObjCRuntime;
+using CoreAnimation;
 
 using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
 using OpenTK.Platform;
 using OpenTK.Platform.iPhoneOS;
+
+using Microsoft.Xna.Framework;
 
 using RectangleF=CoreGraphics.CGRect;
 using SizeF=CoreGraphics.CGSize;
@@ -16,6 +20,9 @@ namespace CocosSharp
 {
     public partial class CCGameView : iPhoneOSGameView
     {
+        bool bufferCreated;
+        uint depthbuffer;
+
         #region Constructors
 
         public CCGameView(NSCoder coder) 
@@ -23,9 +30,8 @@ namespace CocosSharp
         {
             LayerRetainsBacking = true;
             LayerColorFormat = EAGLColorFormat.RGBA8;
+            ContextRenderingApi = EAGLRenderingAPI.OpenGLES2;
             CreateFrameBuffer();
-
-            Initialise();
         }
 
         public CCGameView(RectangleF frame)
@@ -33,9 +39,8 @@ namespace CocosSharp
         {
             LayerRetainsBacking = true;
             LayerColorFormat = EAGLColorFormat.RGBA8;
+            ContextRenderingApi = EAGLRenderingAPI.OpenGLES2;
             CreateFrameBuffer();
-
-            Initialise();
         }
 
         #endregion Constructors
@@ -54,23 +59,63 @@ namespace CocosSharp
 
         protected override void CreateFrameBuffer()
         {
-            ContextRenderingApi = EAGLRenderingAPI.OpenGLES2;
-
             // Fetch desired depth / stencil size
             // Need to more robustly handle
 
-            try {
-                base.CreateFrameBuffer();
-                // Kick start the render loop
-                // In particular, graphics context is lazily created, so we need to start this up 
-                // here so that the view is initialised correctly
-                Run();
+            if (bufferCreated)
                 return;
-            } catch (Exception ex) {      
-            }
+
+            base.CreateFrameBuffer();
+
+            MakeCurrent();
+
+            CAEAGLLayer eaglLayer = (CAEAGLLayer) Layer;
+
+            var newSize = new System.Drawing.Size(
+                (int) Math.Round(eaglLayer.Bounds.Size.Width), 
+                (int) Math.Round(eaglLayer.Bounds.Size.Height));
+
+            GL.GenRenderbuffers(1, out depthbuffer);
+            GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, depthbuffer);
+            GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferInternalFormat.DepthComponent16, newSize.Width, newSize.Height);
+            GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferSlot.DepthAttachment, RenderbufferTarget.Renderbuffer, depthbuffer);
+
+            if (Threading.BackgroundContext == null)
+                Threading.BackgroundContext = new OpenGLES.EAGLContext(EAGLContext.API, EAGLContext.ShareGroup);
+
+            Size = newSize;
+
+            Initialise();
+
+            bufferCreated = true;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            ViewSize = new CCSizeI(Size.Width, Size.Height);
+            viewportDirty = true;
         }
 
         #endregion Initialisation
+
+
+        #region Cleaning up
+
+        protected override void DestroyFrameBuffer()
+        {
+            base.DestroyFrameBuffer();
+
+            MakeCurrent();
+
+            GL.DeleteRenderbuffers (1, ref depthbuffer);
+            depthbuffer = 0;
+
+            bufferCreated = false;
+        }
+
+        #endregion Cleaning up 
 
 
         protected override void OnRenderFrame(FrameEventArgs e)
