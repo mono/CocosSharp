474df3319afd676b29ce3c95a9d0fcef3521fce0
Kenneth Pouncey <kjpou@pt.lu>
2015-07-18 08:29:42 +0200

    Fix potential memory leak in CCLabel-Renderer81.cs.  Wrap the render texture in a using statement to make sure it is being closed and disposed of correctly.  Made RenderLabelToStream async Task.  Use ReadAsync when loading png stream representation of rendered label.

--- src/label_nodes/CCLabel-WindowsPhone81.cs
+++ src/label_nodes/CCLabel-WindowsPhone81.cs
@@ -329,7 +329,7 @@ namespace CocosSharp
                 Texture2D tex = null;
 
                 using (var pngStream = labelRenderer.RenderLabelToStream(imageWidth, imageHeight, foregroundColor,
-                    new Vector2(boundingRect.X, yOffset), textLayout))
+                    new Vector2(boundingRect.X, yOffset), textLayout).Result)
                 {
                     // Create the Texture2D from the png stream
                     tex = Texture2D.FromStream(CCDrawManager.SharedDrawManager.XnaGraphicsDevice, pngStream);
