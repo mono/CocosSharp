9f12b21f85e92e28c2b9fb6e9b9fc11356d7e96a
Kenneth Pouncey <kjpou@pt.lu>
2015-07-29 08:52:22 +0200

    New CCTextField implementation.  Implement iOS IMEKeyboardInput version following ICCIMEDelegate interface.
    See Issue https://github.com/mono/CocosSharp/issues/55

--- src/text_input_node/Android/IMEKeyboardImpl.cs
+++ src/text_input_node/Android/IMEKeyboardImpl.cs
@@ -1,42 +1,3 @@
-#region License
-/*
-Microsoft Public License (Ms-PL)
-MonoGame - Copyright © 2009 The MonoGame Team
-
-All rights reserved.
-
-This license governs use of the accompanying software. If you use the software, you accept this license. If you do not
-accept the license, do not use the software.
-
-1. Definitions
-The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under 
-U.S. copyright law.
-
-A "contribution" is the original software, or any additions or changes to the software.
-A "contributor" is any person that distributes its contribution under this license.
-"Licensed patents" are a contributor's patent claims that read directly on its contribution.
-
-2. Grant of Rights
-(A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, 
-each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
-(B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, 
-each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
-
-3. Conditions and Limitations
-(A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
-(B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, 
-your patent license from such contributor to the software ends automatically.
-(C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution 
-notices that are present in the software.
-(D) If you distribute any portion of the software in source code form, you may do so only under this license by including 
-a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object 
-code form, you may only do so under a license that complies with this license.
-(E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees
-or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent
-permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular
-purpose and non-infringement.
-*/
-#endregion License
 
 #region Using clause
 using System;
@@ -55,16 +16,24 @@ using System.Threading.Tasks;
 
 #endregion Using clause
 
+
 namespace CocosSharp
 {
-
-    public class IMEKeyboardImpl : ICCIMEDelegate
+    /// <summary>
+    /// IME keyboard implementaion for Android.  This class uses an AlerDialog.Builder to create an AlertDialog to be 
+    /// presented for input.
+    /// </summary>
+    internal class IMEKeyboardImpl : ICCIMEDelegate
     {
         private bool isVisible;
         private string contentText;
 
         private static IMEKeyboardImpl instance;
 
+        /// <summary>
+        /// Returns a shared instance of the platform keyboard implemenation
+        /// </summary>
+        /// <value>The shared instance.</value>
         public static IMEKeyboardImpl SharedInstance
         {
             get
@@ -76,6 +45,7 @@ namespace CocosSharp
             }
         }
 
+        // Based on MonoGame's Guide implementation for Android
         AlertDialog alertDialog = null;
         public string ShowKeyboardInput(
          string defaultText)
@@ -288,10 +258,7 @@ namespace CocosSharp
             }
         }
 
-        //////////////////////////////////////////////////////////////////////////
-        // keyboard show/hide notification
-        //////////////////////////////////////////////////////////////////////////
-
+        #region keyboard show/hide notification
         public event EventHandler<CCIMEKeyboardNotificationInfo> KeyboardWillShow;
 
         void OnKeyboardWillShow()
@@ -327,6 +294,7 @@ namespace CocosSharp
             if (handler != null)
                 handler(this, new CCIMEKeyboardNotificationInfo());
         }
+        #endregion
     }
     #endregion
 
