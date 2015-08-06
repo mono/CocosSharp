a0d9fb78333d0afbf711e66eb06d993ec10ffd77
Kenneth Pouncey <kjpou@pt.lu>
2015-08-06 05:38:17 +0200

    Cleanup SpriteTestScene to use Func<CCLayer> to traverse the scenes instead of static switch.  Makes it easier to manage additions, changes and removals of tests.

--- tests/tests/classes/tests/SpriteTest/SpriteTestScene.cs
+++ tests/tests/classes/tests/SpriteTest/SpriteTestScene.cs
@@ -30,6 +30,11 @@ namespace tests
 
     public class SpriteTestScene : TestScene
     {
+
+        public SpriteTestScene() : base()
+        {
+            MAX_LAYER = createSpriteTestLayerFunctions.Length;
+        }
         public override void runThisTest()
         {
             CCLayer layer = NextSpriteTestAction();
@@ -39,72 +44,64 @@ namespace tests
         }
 
         static int sceneIdx = -1;
-        static int MAX_LAYER = 48;
+        static int MAX_LAYER = 0;
+
+        static Func<CCLayer>[] createSpriteTestLayerFunctions =
+            {
+                () => new Sprite1(),
+                () => new SpriteBatchNode1(),
+                () => new SpriteFrameTest(),
+                () => new SpriteFrameAliasNameTest(),
+                () => new SpriteAnchorPoint(),
+                () => new SpriteBatchNodeAnchorPoint(),
+                () => new SpriteOffsetAnchorRotation(),
+                () => new SpriteBatchNodeOffsetAnchorRotation(),
+                () => new SpriteOffsetAnchorScale(),
+                () => new SpriteBatchNodeOffsetAnchorScale(),
+                () => new SpriteOffsetAnchorSkew(),
+                () => new SpriteBatchNodeOffsetAnchorSkew(),
+                () => new SpriteOffsetAnchorSkewScale(),
+                () => new SpriteBatchNodeOffsetAnchorSkewScale(),
+                () => new SpriteAnimationSplit(),
+                () => new SpriteColorOpacity(),
+                () => new SpriteBatchNodeColorOpacity(),
+                () => new SpriteZOrder(),
+                () => new SpriteBatchNodeZOrder(),
+                () => new SpriteBatchNodeReorder(),
+                () => new SpriteBatchNodeReorderIssue744(),
+                () => new SpriteBatchNodeReorderIssue766(),
+                () => new SpriteBatchNodeReorderIssue767(),
+                () => new SpriteZVertex(),
+                () => new SpriteBatchNodeZVertex(),
+                () => new Sprite6(),
+                () => new SpriteFlip(),
+                () => new SpriteBatchNodeFlip(),
+                () => new SpriteAliased(),
+                () => new SpriteBatchNodeAliased(),
+                () => new SpriteNewTexture(),
+                () => new SpriteBatchNodeNewTexture(),
+                () => new SpriteHybrid(),
+                () => new SpriteBatchNodeChildren(),
+                () => new SpriteBatchNodeChildrenZ(),
+                () => new SpriteChildrenVisibility(),
+                () => new SpriteChildrenVisibilityIssue665(),
+                () => new SpriteChildrenAnchorPoint(),
+                () => new SpriteBatchNodeChildrenAnchorPoint(),
+                () => new SpriteBatchNodeChildrenScale(),
+                () => new SpriteChildrenChildren(),
+                () => new SpriteBatchNodeChildrenChildren(),
+                () => new SpriteNilTexture(),
+                () => new SpriteSubclass(),
+                () => new AnimationCache(),
+                () => new SpriteOffsetAnchorFlip(),
+                () => new SpriteBatchNodeOffsetAnchorFlip(),
+                () => new SpriteMaskTest(),
+            };
 
         public static CCLayer createSpriteTestLayer(int index)
         {
-            switch (index)
-            {
-            case 0: return new Sprite1();
-            case 1: return new SpriteBatchNode1();
-            case 2: return new SpriteFrameTest();
-            case 3: return new SpriteFrameAliasNameTest();
-            case 4: return new SpriteAnchorPoint();
-            case 5: return new SpriteBatchNodeAnchorPoint();
-            case 6: return new SpriteOffsetAnchorRotation();
-            case 7: return new SpriteBatchNodeOffsetAnchorRotation();
-            case 8: return new SpriteOffsetAnchorScale();
-            case 9: return new SpriteBatchNodeOffsetAnchorScale();
-            case 10: return new SpriteOffsetAnchorSkew();
-            case 11: return new SpriteBatchNodeOffsetAnchorSkew();
-            case 12: return new SpriteOffsetAnchorSkewScale();
-            case 13: return new SpriteBatchNodeOffsetAnchorSkewScale();
-            case 14: return new SpriteAnimationSplit();
-            case 15: return new SpriteColorOpacity();
-            case 16: return new SpriteBatchNodeColorOpacity();
-            case 17: return new SpriteZOrder();
-            case 18: return new SpriteBatchNodeZOrder();
-            case 19: return new SpriteBatchNodeReorder();
-            case 20: return new SpriteBatchNodeReorderIssue744();
-            case 21: return new SpriteBatchNodeReorderIssue766();
-            case 22: return new SpriteBatchNodeReorderIssue767();
-            case 23: return new SpriteZVertex();
-            case 24: return new SpriteBatchNodeZVertex();
-            case 25: return new Sprite6();
-            case 26: return new SpriteFlip();
-            case 27: return new SpriteBatchNodeFlip();
-            case 28: return new SpriteAliased();
-            case 29: return new SpriteBatchNodeAliased();
-            case 30: return new SpriteNewTexture();
-            case 31: return new SpriteBatchNodeNewTexture();
-            case 32: return new SpriteHybrid();
-            case 33: return new SpriteBatchNodeChildren();
-            case 34: return new SpriteBatchNodeChildrenZ();
-            case 35: return new SpriteChildrenVisibility();
-            case 36: return new SpriteChildrenVisibilityIssue665();
-            case 37: return new SpriteChildrenAnchorPoint();
-            case 38: return new SpriteBatchNodeChildrenAnchorPoint();
-            case 39: return new SpriteBatchNodeChildrenScale();
-            case 40: return new SpriteChildrenChildren();
-            case 41: return new SpriteBatchNodeChildrenChildren();
-            case 42: return new SpriteNilTexture();
-            case 43: return new SpriteSubclass();
-            case 44: return new AnimationCache();
-            case 45: return new SpriteOffsetAnchorFlip();
-            case 46: return new SpriteBatchNodeOffsetAnchorFlip();
-            case 47: return new SpriteMaskTest();
+            return createSpriteTestLayerFunctions[index]();
 
-                //case 47: return new SpriteBatchNodeReorderSameIndex();
-                //case 48: return new SpriteBatchNodeReorderOneChild();
-                //case 49: return new NodeSort();
-                //case 50: return new SpriteSkewNegativeScaleChildren();
-                //case 51: return new SpriteBatchNodeSkewNegativeScaleChildren();
-                //case 52: return new SpriteDoubleResolution();
-                //case 53: return new SpriteBatchBug1217();
-                //case 54: return new AnimationCacheFile();
-            }
-
-            return null;
         }
 
         protected override void NextTestCase()
