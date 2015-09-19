60a98035cff4454049ba431360ae6747a5164198
Rami Tabbara <rtabbara@gmail.com>
2015-09-19 17:21:32 +1000

    Forms: CCGameView.WP: Pause calls mobile-specific changes

--- src/EmbeddableView/CCGameView.WindowsPhone.cs
+++ src/EmbeddableView/CCGameView.WindowsPhone.cs
@@ -93,6 +93,8 @@ namespace CocosSharp
                 CompositionTarget.Rendering -= OnUpdateFrame;
             else
                 CompositionTarget.Rendering += OnUpdateFrame;
+
+            MobilePlatformUpdatePaused();
         }
 
         void ViewStateChanged(object sender, WindowActivatedEventArgs e)
