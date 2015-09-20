0e920c3340c63593a13f80a2f506072624218e79
Rami Tabbara <rtabbara@gmail.com>
2015-09-20 16:35:21 +1000

    Forms: CCGameView: Removing reference to obsolete CocosDenshion namespace

--- src/EmbeddableView/CCGameView.cs
+++ src/EmbeddableView/CCGameView.cs
@@ -8,8 +8,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input.Touch;
 using Microsoft.Xna.Framework.Audio;
 
-using CocosDenshion;
-
 namespace CocosSharp
 {
     using XnaSurfaceFormat = Microsoft.Xna.Framework.Graphics.SurfaceFormat;
