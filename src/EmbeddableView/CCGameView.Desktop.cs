1921deede893e0cd326e99f07c8ab613ce85ee9b
kjpou1 <kjpou@pt.lu>
2015-10-09 10:58:41 +0200

    Remove redundant code made to CCGameView.Desktop.cs

--- src/EmbeddableView/CCGameView.Desktop.cs
+++ src/EmbeddableView/CCGameView.Desktop.cs
@@ -6,171 +6,72 @@ namespace CocosSharp
 {
     public partial class CCGameView
     {
-        static readonly TimeSpan TouchTimeLimit = TimeSpan.FromMilliseconds(1000);
 
-        bool touchEnabled;
-        bool prevAccelerometerEnabled;
+        bool mouseEnabled;
 
-        Dictionary<int, CCTouch> touchMap;
-        List<CCTouch> incomingNewTouches;
-        List<CCTouch> incomingMoveTouches;
-        List<CCTouch> incomingReleaseTouches;
+        Dictionary<int, CCEventMouse> mouseMap;
+        List<CCEventMouse> incomingMoveMouse;
 
-        object touchLock = new object();
+        object mouseLock = new object();
 
         #region Properties
 
-        public bool TouchEnabled
+        public bool MouseEnabled
         {
-            get { return touchEnabled; }
+            get { return mouseEnabled; }
             set
             {
-                touchEnabled = value;
-                PlatformUpdateTouchEnabled();
+                mouseEnabled = value;
+                PlatformUpdateMouseEnabled();
             }
         }
-
-        public CCAccelerometer Accelerometer { get; private set; }
 
         #endregion Properties
 
 
         #region Initialisation
 
-        void InitialiseInputHandling()
+        void InitialiseMouseHandling()
         {
-            touchMap = new Dictionary<int, CCTouch>();
-            incomingNewTouches = new List<CCTouch>();
-            incomingMoveTouches = new List<CCTouch>();
-            incomingReleaseTouches = new List<CCTouch>();
+            mouseMap = new Dictionary<int, CCEventMouse>();
+            incomingMoveMouse = new List<CCEventMouse>();
 
-            TouchEnabled = true;
+            MouseEnabled = CCDevice.IsMousePresent;
 
-            Accelerometer = new CCAccelerometer(this);
         }
 
         #endregion Initialisation
 
 
-        #region Accelerometer handling
+        #region Mouse handling
 
-        public void MobilePlatformUpdatePaused()
+        void UpdateIncomingMoveMouse(int touchId, ref CCPoint position)
         {
-            if (Paused) 
+            lock (mouseLock)
             {
-                prevAccelerometerEnabled = Accelerometer.Enabled;
-                Accelerometer.Enabled = false;
-            } 
-            else
-                Accelerometer.Enabled = prevAccelerometerEnabled;
-        }
+                var mouse = new CCEventMouse(CCMouseEventType.MOUSE_MOVE, position);
+                incomingMoveMouse.Add(mouse);
 
-        #endregion Accelerometer handling
-
-
-        #region Touch handling
-
-        void AddIncomingNewTouch(int touchId, ref CCPoint position)
-        {
-            lock (touchLock) 
-            {
-                if (!touchMap.ContainsKey (touchId)) 
-                {
-                    var touch = new CCTouch (touchId, position, gameTime.TotalGameTime);
-                    touchMap.Add (touchId, touch);
-                    incomingNewTouches.Add (touch);
-                }
             }
         }
 
-        void UpdateIncomingMoveTouch(int touchId, ref CCPoint position)
+        void ProcessMouseInput()
         {
-            lock (touchLock) 
+            lock (mouseLock) 
             {
-                CCTouch existingTouch;
-                if (touchMap.TryGetValue (touchId, out existingTouch)) 
+                if (EventDispatcher.IsEventListenersFor(CCEventListenerMouse.LISTENER_ID))
                 {
-                    var delta = existingTouch.LocationOnScreen - position;
-                    if (delta.LengthSquared > 1.0f) 
+                    if (incomingMoveMouse.Count > 0)
                     {
-                        incomingMoveTouches.Add (existingTouch);
-                        existingTouch.UpdateTouchInfo (touchId, position.X, position.Y, gameTime.TotalGameTime);
+                        EventDispatcher.DispatchEvent(incomingMoveMouse[0]);
                     }
+
+                    incomingMoveMouse.Clear();
                 }
             }
         }
 
-        void UpdateIncomingReleaseTouch(int touchId)
-        {
-            lock (touchLock) 
-            {
-                CCTouch existingTouch;
-                if (touchMap.TryGetValue (touchId, out existingTouch)) 
-                {
-                    incomingReleaseTouches.Add (existingTouch);
-                    touchMap.Remove (touchId);
-                }
-            }
-        }
-
-        void ProcessInput()
-        {
-            lock (touchLock) 
-            {
-                if (EventDispatcher.IsEventListenersFor (CCEventListenerTouchOneByOne.LISTENER_ID)
-                || EventDispatcher.IsEventListenersFor (CCEventListenerTouchAllAtOnce.LISTENER_ID)) 
-                {
-                    var touchEvent = new CCEventTouch (CCEventCode.BEGAN);
-
-                    RemoveOldTouches ();
-
-                    if (incomingNewTouches.Count > 0) {
-                        touchEvent.EventCode = CCEventCode.BEGAN;
-                        touchEvent.Touches = incomingNewTouches;
-                        EventDispatcher.DispatchEvent (touchEvent);
-                    }
-
-                    if (incomingMoveTouches.Count > 0) {
-                        touchEvent.EventCode = CCEventCode.MOVED;
-                        touchEvent.Touches = incomingMoveTouches;
-                        EventDispatcher.DispatchEvent (touchEvent);
-                    }
-
-                    if (incomingReleaseTouches.Count > 0) {
-                        touchEvent.EventCode = CCEventCode.ENDED;
-                        touchEvent.Touches = incomingReleaseTouches;
-                        EventDispatcher.DispatchEvent (touchEvent);
-                    }
-
-                    incomingNewTouches.Clear ();
-                    incomingMoveTouches.Clear ();
-                    incomingReleaseTouches.Clear ();
-                }
-            }
-        }
-
-        // Prevent memory leaks by removing stale touches
-        // In particular, in the case of the game entering the background
-        // a release touch event may not have been triggered within the view
-        void RemoveOldTouches()
-        {
-            lock (touchLock) 
-            {
-                var currentTime = gameTime.ElapsedGameTime;
-
-                foreach (CCTouch touch in touchMap.Values) 
-                {
-                    if (!incomingReleaseTouches.Contains (touch)
-                    && (currentTime - touch.TimeStamp) > TouchTimeLimit) 
-                    {
-                        incomingReleaseTouches.Add (touch);
-                        touchMap.Remove (touch.Id);
-                    }
-                }
-            }
-        }
-
-        #endregion Touch handling
+        #endregion Mouse handling
     }
 }
 
--- src/EmbeddableView/CCGameView.WindowsRT.cs
+++ src/EmbeddableView/CCGameView.WindowsRT.cs
@@ -12,170 +12,43 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace CocosSharp
 {
-    public class GameSwapChainPanel : SwapChainPanel
+
+    public partial class CCGameView 
     {
-        protected virtual void Dispose(bool disposing)
+
+        #region Mouse handling
+
+        PointerEventHandler mouseMovedHandler;
+
+        void PlatformUpdateMouseEnabled()
         {
-        }
-    }
+            if (mouseMovedHandler == null)
+                mouseMovedHandler = new PointerEventHandler(MouseMoved);
 
-    public partial class CCGameView : GameSwapChainPanel
-    {
-        #region Constructors
-
-        public CCGameView()
-        {
-            Initialise();
-        }
-
-        #endregion Constructors
-
-
-        #region Initialisation
-
-        void PlatformInitialise()
-        {
-            SizeChanged += ViewSizeChanged;
-            Window.Current.Activated += ViewStateChanged;
-        }
-
-        void PlatformInitialiseGraphicsDevice(ref PresentationParameters presParams)
-        {
-            presParams.SwapChainBackgroundPanel = this;
-        }
-
-        void PlatformStartGame()
-        {
-            CompositionTarget.Rendering += OnUpdateFrame;
-            viewportDirty = true;
-        }
-
-        #endregion Initialisation
-
-
-        #region Cleaning up
-
-        void PlatformDispose(bool disposing)
-        {
-            Window.Current.Activated -= ViewStateChanged;
-        }
-
-        #endregion Cleaning up
-
-
-        #region Rendering
-
-        void ViewSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            ViewSize = new CCSizeI((int)e.NewSize.Width, (int)e.NewSize.Height);
-            
-            // We need to ask the graphics device to resize the underlying swapchain/buffers
-            graphicsDevice.PresentationParameters.BackBufferWidth = ViewSize.Width;
-            graphicsDevice.PresentationParameters.BackBufferHeight = ViewSize.Height;
-            graphicsDevice.CreateSizeDependentResources();
-            graphicsDevice.ApplyRenderTargets(null);
-
-            UpdateViewport();
-
-            platformInitialised = true;
-
-            LoadGame();
-        }
-
-        #endregion Rendering
-
-
-        #region Run loop
-
-        void PlatformUpdatePaused()
-        {
-            if (Paused)
-                CompositionTarget.Rendering -= OnUpdateFrame;
-            else
-                CompositionTarget.Rendering += OnUpdateFrame;
-
-            MobilePlatformUpdatePaused();
-        }
-
-        void ViewStateChanged(object sender, WindowActivatedEventArgs e)
-        {
-            Paused = (e.WindowActivationState == CoreWindowActivationState.Deactivated);
-        }
-
-        void OnUpdateFrame(object sender, object e)
-        {
-            Tick();
-
-            graphicsDevice.ResetRenderTargets();
-
-            Draw();
-
-            PlatformPresent();
-        }
-
-        void PlatformPresent()
-        {
-            if (graphicsDevice != null)
-                graphicsDevice.Present();
-        }
-
-        #endregion Run loop
-
-
-        #region Touch handling
-
-        void PlatformUpdateTouchEnabled()
-        {
-            if (TouchEnabled)
+            if (MouseEnabled)
             {
-                PointerPressed += TouchesBegan;
-                PointerReleased += TouchesEnded;
-                PointerCanceled += TouchesEnded;
-                PointerMoved += TouchesMoved;
+                AddHandler(UIElement.PointerMovedEvent, mouseMovedHandler, true);
             }
             else
             {
-                PointerPressed -= TouchesBegan;
-                PointerReleased -= TouchesEnded;
-                PointerCanceled -= TouchesEnded;
-                PointerMoved -= TouchesMoved;
+                RemoveHandler(UIElement.PointerMovedEvent, mouseMovedHandler);
             }
         }
 
-        void TouchesBegan(object sender, PointerRoutedEventArgs args)
-        {
-            ((UIElement)sender).CapturePointer(args.Pointer);
-
-            var pointerPoint = args.GetCurrentPoint(this);
-            var pos = new CCPoint((float)pointerPoint.Position.X, (float)pointerPoint.Position.Y);
-
-            AddIncomingNewTouch((int)pointerPoint.PointerId, ref pos);
-
-            args.Handled = true;
-        }
-
-        void TouchesMoved(object sender, PointerRoutedEventArgs args)
+        void MouseMoved(object sender, PointerRoutedEventArgs args)
         {
             var pointerPoint = args.GetCurrentPoint(this);
-            var pos = new CCPoint((float)pointerPoint.Position.X, (float)pointerPoint.Position.Y);
+            if (pointerPoint.PointerDevice.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                
+                var pos = new CCPoint((float)pointerPoint.Position.X, (float)pointerPoint.Position.Y);
 
-            UpdateIncomingMoveTouch((int)pointerPoint.PointerId, ref pos);
-            UpdateIncomingMoveMouse((int)pointerPoint.PointerId, ref pos);
+                UpdateIncomingMoveMouse((int)pointerPoint.PointerId, ref pos);
 
-            args.Handled = true;
+                args.Handled = true;
+            }
         }
 
-        void TouchesEnded(object sender, PointerRoutedEventArgs args)
-        {
-            ((UIElement)sender).ReleasePointerCapture(args.Pointer);
-
-            var pointerPoint = args.GetCurrentPoint(this);
-
-            UpdateIncomingReleaseTouch((int)pointerPoint.PointerId);
-
-            args.Handled = true;
-        }
-
-        #endregion Touch handling
+        #endregion Mouse handling
     }
 }
