1073d8be32fcf43398a988598f1b6b38329c7405
Rami Tabbara <rtabbara@gmail.com>
2015-10-10 13:56:08 +1100

    CocosSharp.Forms: Initial solution commit
    * Still need to incorporate WindowsPhone projects

---
+++ CocosSharp.Forms/CocosSharp.Forms.PCL/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle ("CocosSharp.Forms.PCL")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCompany("Open Source Software Provided As-Is")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyInformationalVersion("1.6.2.0")]
