f4e41b0cebbb418ee19822172587495decf4515d
kjpou1 <kjpou@pt.lu>
2015-10-12 08:59:29 +0200

    [cleanup] Fix compile error AssemblyVersion("1.7.0.0-pre1") -> AssemblyVersion("1.7.0.0")

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.0.0-pre1")]
+[assembly: AssemblyVersion("1.7.0.0")]
 [assembly: AssemblyInformationalVersion("1.7.0.0-pre1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
