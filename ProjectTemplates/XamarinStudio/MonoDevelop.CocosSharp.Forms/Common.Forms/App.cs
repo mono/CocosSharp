58bdab53960c518e2813919d79c613622e868b4d
Rami Tabbara <rtabbara@gmail.com>
2015-10-23 01:07:00 +1100

    Add CocosSharp.Forms XS templates

---
+++ ProjectTemplates/XamarinStudio/MonoDevelop.CocosSharp.Forms/Common.Forms/App.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using CocosSharp;
+
+namespace ${Namespace}
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new CocosSharpView()
+				{
+					HorizontalOptions = LayoutOptions.FillAndExpand,
+					VerticalOptions = LayoutOptions.FillAndExpand,
+					// Set the game world dimensions
+					DesignResolution = new Size(1024, 768),
+					// Set the method to call once the view has been initialised
+					ViewCreated = LoadGame
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+		
+		
+		void LoadGame (object sender, EventArgs e)
+		{
+			CCGameView gameView = sender as CCGameView;
+
+			if (gameView != null) {
+				var contentSearchPaths = new List<string> () { "Fonts", "Sounds" };
+				CCSizeI viewSize = gameView.ViewSize;
+				CCSizeI designResolution = gameView.DesignResolution;
+
+				// Determine whether to use the high or low def versions of our images
+				// Make sure the default texel to content size ratio is set correctly
+				// Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+				if (designResolution.Width < viewSize.Width) {
+					contentSearchPaths.Add ("Images/Hd");
+					CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+				} else {
+					contentSearchPaths.Add ("Images/Ld");
+					CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+				}
+
+				gameView.ContentManager.SearchPaths = contentSearchPaths;
+
+				CCScene gameScene = new CCScene (gameView);
+				gameScene.AddLayer (new GameLayer ());
+				gameView.RunWithScene (gameScene);
+			}
+		}
+	}
+}
