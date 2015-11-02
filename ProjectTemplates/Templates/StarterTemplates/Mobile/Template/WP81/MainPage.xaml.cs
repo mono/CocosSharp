b24de6bbe727a7f73adb85e946cb8d83d72d2185
Kenneth Pouncey <kjpou@pt.lu>
2015-11-02 07:36:57 +0100

    [VS Templates - WP8.1] Change WP8.1 CocosSharp Shared template to use Embedded CCGameView.
    + Use GameDelegate.LoadGame common logic from the shared project as the CCGameView.ViewCreated delegate.

---
+++ ProjectTemplates/Templates/StarterTemplates/Mobile/Template/WP81/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MonoGame.Framework;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+using CocosSharp;
+
+using $ext_projectname$.Shared;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            GameView.ViewCreated += GameDelegate.LoadGame;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        void LoadGame(object sender, EventArgs e)
+        {
+            CCGameView gameView = sender as CCGameView;
+
+            if (gameView != null)
+            {
+                var contentSearchPaths = new List<string>() { "Fonts", "Sounds" };
+                CCSizeI viewSize = gameView.ViewSize;
+
+                int width = 1024;
+                int height = 768;
+
+                // Set world dimensions
+                gameView.DesignResolution = new CCSizeI(width, height);
+
+                // Determine whether to use the high or low def versions of our images
+                // Make sure the default texel to content size ratio is set correctly
+                // Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+                if (width < viewSize.Width)
+                {
+                    contentSearchPaths.Add("Images/Hd");
+                    CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+                }
+                else
+                {
+                    contentSearchPaths.Add("Images/Ld");
+                    CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+                }
+
+                gameView.ContentManager.SearchPaths = contentSearchPaths;
+
+                CCScene gameScene = new CCScene(gameView);
+                gameScene.AddLayer(new GameLayer());
+                gameView.RunWithScene(gameScene);
+            }
+        }
+    }
+}
