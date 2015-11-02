515b1d2a50a1545d4facf6e13d4f631521616aaa
Rami Tabbara <rtabbara@gmail.com>
2015-11-02 15:28:18 +1100

    XS templates: Update Xam.Forms templates to use GamePage

--- ProjectTemplates/XamarinStudio/MonoDevelop.CocosSharp.Forms/Common.Forms/App.cs
+++ ProjectTemplates/XamarinStudio/MonoDevelop.CocosSharp.Forms/Common.Forms/App.cs
@@ -10,17 +10,7 @@ namespace ${Namespace}
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new CocosSharpView()
-				{
-					HorizontalOptions = LayoutOptions.FillAndExpand,
-					VerticalOptions = LayoutOptions.FillAndExpand,
-					// Set the game world dimensions
-					DesignResolution = new Size(1024, 768),
-					// Set the method to call once the view has been initialised
-					ViewCreated = LoadGame
-				}
-			};
+			MainPage = new GamePage();
 		}
 
 		protected override void OnStart ()
@@ -36,35 +26,6 @@ namespace ${Namespace}
 		protected override void OnResume ()
 		{
 			// Handle when your app resumes
-		}
-		
-		
-		void LoadGame (object sender, EventArgs e)
-		{
-			CCGameView gameView = sender as CCGameView;
-
-			if (gameView != null) {
-				var contentSearchPaths = new List<string> () { "Fonts", "Sounds" };
-				CCSizeI viewSize = gameView.ViewSize;
-				CCSizeI designResolution = gameView.DesignResolution;
-
-				// Determine whether to use the high or low def versions of our images
-				// Make sure the default texel to content size ratio is set correctly
-				// Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
-				if (designResolution.Width < viewSize.Width) {
-					contentSearchPaths.Add ("Images/Hd");
-					CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
-				} else {
-					contentSearchPaths.Add ("Images/Ld");
-					CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
-				}
-
-				gameView.ContentManager.SearchPaths = contentSearchPaths;
-
-				CCScene gameScene = new CCScene (gameView);
-				gameScene.AddLayer (new GameLayer ());
-				gameView.RunWithScene (gameScene);
-			}
 		}
 	}
 }
