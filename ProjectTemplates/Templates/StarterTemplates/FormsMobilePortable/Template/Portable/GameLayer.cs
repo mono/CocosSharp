7fb8102859ca51079fb7ecd471d80f93527b3bb1
kjpou1 <kjpou@pt.lu>
2015-11-04 15:48:36 +0100

    [VS Templates - Forms Portable] New Xamarin.Forms templates for a Portable project that uses a CocosSharpView.
    + Portable project that includes the common code modules used by each platform
    + Android, iOS and Windows Phone 8.1 Xamarin.Forms blank projects that reference the Portable common project code.

---
+++ ProjectTemplates/Templates/StarterTemplates/FormsMobilePortable/Template/Portable/GameLayer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using CocosSharp;
+using Microsoft.Xna.Framework;
+
+namespace $safeprojectname$
+{
+    public class GameLayer : CCLayerColor
+    {
+
+        // Define a label variable
+        CCLabel label;
+
+        public GameLayer() : base(CCColor4B.Blue)
+        {
+
+            // create and initialize a Label
+            label = new CCLabel("Hello CocosSharp", "Fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
+
+            // add the label as a child to this Layer
+            AddChild(label);
+
+        }
+
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+
+            // Use the bounds to layout the positioning of our drawable assets
+            var bounds = VisibleBoundsWorldspace;
+
+            // position the label on the center of the screen
+            label.Position = bounds.Center;
+
+            // Register for touch events
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesEnded = OnTouchesEnded;
+            AddEventListener(touchListener, this);
+        }
+
+        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            if (touches.Count > 0)
+            {
+                // Perform touch handling here
+            }
+        }
+	}
+}
+
