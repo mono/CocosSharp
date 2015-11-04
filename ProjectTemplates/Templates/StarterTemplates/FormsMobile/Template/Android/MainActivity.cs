309efe5aec22808702a82d313cbcbd9a65e2e7c8
Kenneth Pouncey <kjpou@pt.lu>
2015-11-04 14:21:45 +0100

    [VS Templates - Forms Shared] New Xamarin.Forms templates for a Shared project that uses a CocosSharpView.
    + Shared project that includes the common code modules used by each platform
    + Android, iOS and Windows Phone 8.1 Xamarin.Forms blank projects that reference the Shared common project code.

---
+++ ProjectTemplates/Templates/StarterTemplates/FormsMobile/Template/Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using CocosSharp;
+using $ext_projectname$.Shared;
+
+namespace $safeprojectname$
+{
+	[Activity (Label = "$safeprojectname$", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new App ());
+		}
+	}
+}
+
---
+++ ProjectTemplates/Templates/StarterTemplates/FormsMobile/Template/iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+using Foundation;
+using UIKit;
+
+using $ext_projectname$.Shared;
+
+namespace $safeprojectname$
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : 
+	global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate 
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			LoadApplication (new App ());  
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
